5393ce21fd4fab37c6ea27207b19d4326bca5a03
Rushmead <Rushmead@techie.com>
2013-04-14 01:41:16 -0700

    Add Old Code Which Should Work Out!

--- src/minecraft/ljdp/minechem/client/render/RenderTableOfElements.java
+++ src/minecraft/ljdp/minechem/client/render/RenderTableOfElements.java
@@ -1,145 +1,135 @@
 package ljdp.minechem.client.render;
 
 import ljdp.minechem.common.entity.EntityTableOfElements;
-import ljdp.minechem.common.utils.ConstantValue;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityPainting;
 import net.minecraft.util.EnumArt;
 import net.minecraft.util.MathHelper;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
 
-@SideOnly(Side.CLIENT)
-public class RenderTableOfElements extends Render
-{
-    public void renderThePainting(EntityTableOfElements par1EntityPainting, double par2, double par4, double par6, float par8, float par9)
-    {
-        GL11.glPushMatrix();
-        GL11.glTranslatef((float)par2, (float)par4, (float)par6);
-        GL11.glRotatef(par8, 0.0F, 1.0F, 0.0F);
+public class RenderTableOfElements extends Render {
+
+	@Override
+	public void doRender(Entity entity, double x, double y, double z, float angle, float scale) {
+		this.renderPeriodicTable((EntityTableOfElements)entity, x, y, z, angle, scale);
+	}
+	
+	public void renderPeriodicTable(EntityTableOfElements periodicTable, double x, double y, double z, float angle, float scale) {
+		GL11.glPushMatrix();
+        GL11.glTranslatef((float)x, (float)y, (float)z);
+        GL11.glRotatef(angle, 0.0F, 1.0F, 0.0F);
         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        this.loadTexture(ConstantValue.table_HEX);
-        float f2 = 0.0625F;
-        GL11.glScalef(f2, f2, f2);
-        this.func_77010_a(par1EntityPainting, par1EntityPainting.tableSizeX, par1EntityPainting.tableSizeY, 0, 0);
+        this.loadTexture("/art/kz.png");
+        EnumArt art = periodicTable.art;
+        float artScale = 0.0625F;
+        GL11.glScalef(artScale, artScale, artScale);
+        this.renderArt(periodicTable, art.sizeX, art.sizeY, art.offsetX, art.offsetY);
         GL11.glDisable(GL12.GL_RESCALE_NORMAL);
         GL11.glPopMatrix();
-    }
+	}
 
-    private void func_77010_a(EntityTableOfElements par1EntityPainting, int par2, int par3, int par4, int par5)
-    {
-        float f = (float)(-par2) / 2.0F;
-        float f1 = (float)(-par3) / 2.0F;
-        float f2 = 0.5F;
-        float f3 = 0.75F;
-        float f4 = 0.8125F;
-        float f5 = 0.0F;
-        float f6 = 0.0625F;
-        float f7 = 0.75F;
-        float f8 = 0.8125F;
-        float f9 = 0.001953125F;
-        float f10 = 0.001953125F;
-        float f11 = 0.7519531F;
-        float f12 = 0.7519531F;
-        float f13 = 0.0F;
-        float f14 = 0.0625F;
+	private void renderArt(EntityTableOfElements periodicTable, int sizeX, int sizeY, int offsetX, int offsetY) {
+		float var6 = (float)(-sizeX) / 2.0F;
+        float var7 = (float)(-sizeY) / 2.0F;
+        float var8 = 0.5F;
+        float var9 = 0.75F;
+        float var10 = 0.8125F;
+        float var11 = 0.0F;
+        float var12 = 0.0625F;
+        float var13 = 0.75F;
+        float var14 = 0.8125F;
+        float var15 = 0.001953125F;
+        float var16 = 0.001953125F;
+        float var17 = 0.7519531F;
+        float var18 = 0.7519531F;
+        float var19 = 0.0F;
+        float var20 = 0.0625F;
 
-        for (int i1 = 0; i1 < par2 / 16; ++i1)
+        for (int var21 = 0; var21 < sizeX / 16; ++var21)
         {
-            for (int j1 = 0; j1 < par3 / 16; ++j1)
+            for (int var22 = 0; var22 < sizeY / 16; ++var22)
             {
-                float f15 = f + (float)((i1 + 1) * 16);
-                float f16 = f + (float)(i1 * 16);
-                float f17 = f1 + (float)((j1 + 1) * 16);
-                float f18 = f1 + (float)(j1 * 16);
-                this.func_77008_a(par1EntityPainting, (f15 + f16) / 2.0F, (f17 + f18) / 2.0F);
-                float f19 = (float)(par4 + par2 - i1 * 16) / 256.0F;
-                float f20 = (float)(par4 + par2 - (i1 + 1) * 16) / 256.0F;
-                float f21 = (float)(par5 + par3 - j1 * 16) / 256.0F;
-                float f22 = (float)(par5 + par3 - (j1 + 1) * 16) / 256.0F;
-                Tessellator tessellator = Tessellator.instance;
-                tessellator.startDrawingQuads();
-                tessellator.setNormal(0.0F, 0.0F, -1.0F);
-                tessellator.addVertexWithUV((double)f15, (double)f18, (double)(-f2), (double)f20, (double)f21);
-                tessellator.addVertexWithUV((double)f16, (double)f18, (double)(-f2), (double)f19, (double)f21);
-                tessellator.addVertexWithUV((double)f16, (double)f17, (double)(-f2), (double)f19, (double)f22);
-                tessellator.addVertexWithUV((double)f15, (double)f17, (double)(-f2), (double)f20, (double)f22);
-                tessellator.setNormal(0.0F, 0.0F, 1.0F);
-                tessellator.addVertexWithUV((double)f15, (double)f17, (double)f2, (double)f3, (double)f5);
-                tessellator.addVertexWithUV((double)f16, (double)f17, (double)f2, (double)f4, (double)f5);
-                tessellator.addVertexWithUV((double)f16, (double)f18, (double)f2, (double)f4, (double)f6);
-                tessellator.addVertexWithUV((double)f15, (double)f18, (double)f2, (double)f3, (double)f6);
-                tessellator.setNormal(0.0F, 1.0F, 0.0F);
-                tessellator.addVertexWithUV((double)f15, (double)f17, (double)(-f2), (double)f7, (double)f9);
-                tessellator.addVertexWithUV((double)f16, (double)f17, (double)(-f2), (double)f8, (double)f9);
-                tessellator.addVertexWithUV((double)f16, (double)f17, (double)f2, (double)f8, (double)f10);
-                tessellator.addVertexWithUV((double)f15, (double)f17, (double)f2, (double)f7, (double)f10);
-                tessellator.setNormal(0.0F, -1.0F, 0.0F);
-                tessellator.addVertexWithUV((double)f15, (double)f18, (double)f2, (double)f7, (double)f9);
-                tessellator.addVertexWithUV((double)f16, (double)f18, (double)f2, (double)f8, (double)f9);
-                tessellator.addVertexWithUV((double)f16, (double)f18, (double)(-f2), (double)f8, (double)f10);
-                tessellator.addVertexWithUV((double)f15, (double)f18, (double)(-f2), (double)f7, (double)f10);
-                tessellator.setNormal(-1.0F, 0.0F, 0.0F);
-                tessellator.addVertexWithUV((double)f15, (double)f17, (double)f2, (double)f12, (double)f13);
-                tessellator.addVertexWithUV((double)f15, (double)f18, (double)f2, (double)f12, (double)f14);
-                tessellator.addVertexWithUV((double)f15, (double)f18, (double)(-f2), (double)f11, (double)f14);
-                tessellator.addVertexWithUV((double)f15, (double)f17, (double)(-f2), (double)f11, (double)f13);
-                tessellator.setNormal(1.0F, 0.0F, 0.0F);
-                tessellator.addVertexWithUV((double)f16, (double)f17, (double)(-f2), (double)f12, (double)f13);
-                tessellator.addVertexWithUV((double)f16, (double)f18, (double)(-f2), (double)f12, (double)f14);
-                tessellator.addVertexWithUV((double)f16, (double)f18, (double)f2, (double)f11, (double)f14);
-                tessellator.addVertexWithUV((double)f16, (double)f17, (double)f2, (double)f11, (double)f13);
-                tessellator.draw();
+                float var23 = var6 + (float)((var21 + 1) * 16);
+                float var24 = var6 + (float)(var21 * 16);
+                float var25 = var7 + (float)((var22 + 1) * 16);
+                float var26 = var7 + (float)(var22 * 16);
+                this.setLighting(periodicTable, (var23 + var24) / 2.0F, (var25 + var26) / 2.0F);
+                float var27 = (float)(offsetX + sizeX - var21 * 16) / 256.0F;
+                float var28 = (float)(offsetX + sizeX - (var21 + 1) * 16) / 256.0F;
+                float var29 = (float)(offsetY + sizeY - var22 * 16) / 256.0F;
+                float var30 = (float)(offsetY + sizeY - (var22 + 1) * 16) / 256.0F;
+                Tessellator var31 = Tessellator.instance;
+                var31.startDrawingQuads();
+                var31.setNormal(0.0F, 0.0F, -1.0F);
+                var31.addVertexWithUV((double)var23, (double)var26, (double)(-var8), (double)var28, (double)var29);
+                var31.addVertexWithUV((double)var24, (double)var26, (double)(-var8), (double)var27, (double)var29);
+                var31.addVertexWithUV((double)var24, (double)var25, (double)(-var8), (double)var27, (double)var30);
+                var31.addVertexWithUV((double)var23, (double)var25, (double)(-var8), (double)var28, (double)var30);
+                var31.setNormal(0.0F, 0.0F, 1.0F);
+                var31.addVertexWithUV((double)var23, (double)var25, (double)var8, (double)var9, (double)var11);
+                var31.addVertexWithUV((double)var24, (double)var25, (double)var8, (double)var10, (double)var11);
+                var31.addVertexWithUV((double)var24, (double)var26, (double)var8, (double)var10, (double)var12);
+                var31.addVertexWithUV((double)var23, (double)var26, (double)var8, (double)var9, (double)var12);
+                var31.setNormal(0.0F, 1.0F, 0.0F);
+                var31.addVertexWithUV((double)var23, (double)var25, (double)(-var8), (double)var13, (double)var15);
+                var31.addVertexWithUV((double)var24, (double)var25, (double)(-var8), (double)var14, (double)var15);
+                var31.addVertexWithUV((double)var24, (double)var25, (double)var8, (double)var14, (double)var16);
+                var31.addVertexWithUV((double)var23, (double)var25, (double)var8, (double)var13, (double)var16);
+                var31.setNormal(0.0F, -1.0F, 0.0F);
+                var31.addVertexWithUV((double)var23, (double)var26, (double)var8, (double)var13, (double)var15);
+                var31.addVertexWithUV((double)var24, (double)var26, (double)var8, (double)var14, (double)var15);
+                var31.addVertexWithUV((double)var24, (double)var26, (double)(-var8), (double)var14, (double)var16);
+                var31.addVertexWithUV((double)var23, (double)var26, (double)(-var8), (double)var13, (double)var16);
+                var31.setNormal(-1.0F, 0.0F, 0.0F);
+                var31.addVertexWithUV((double)var23, (double)var25, (double)var8, (double)var18, (double)var19);
+                var31.addVertexWithUV((double)var23, (double)var26, (double)var8, (double)var18, (double)var20);
+                var31.addVertexWithUV((double)var23, (double)var26, (double)(-var8), (double)var17, (double)var20);
+                var31.addVertexWithUV((double)var23, (double)var25, (double)(-var8), (double)var17, (double)var19);
+                var31.setNormal(1.0F, 0.0F, 0.0F);
+                var31.addVertexWithUV((double)var24, (double)var25, (double)(-var8), (double)var18, (double)var19);
+                var31.addVertexWithUV((double)var24, (double)var26, (double)(-var8), (double)var18, (double)var20);
+                var31.addVertexWithUV((double)var24, (double)var26, (double)var8, (double)var17, (double)var20);
+                var31.addVertexWithUV((double)var24, (double)var25, (double)var8, (double)var17, (double)var19);
+                var31.draw();
             }
         }
-    }
+	}
 
-    private void func_77008_a(EntityTableOfElements par1EntityPainting, float par2, float par3)
-    {
-        int i = MathHelper.floor_double(par1EntityPainting.posX);
-        int j = MathHelper.floor_double(par1EntityPainting.posY + (double)(par3 / 16.0F));
-        int k = MathHelper.floor_double(par1EntityPainting.posZ);
+	private void setLighting(EntityTableOfElements periodicTable, float par2, float par3) {
+		int var4 = MathHelper.floor_double(periodicTable.posX);
+        int var5 = MathHelper.floor_double(periodicTable.posY + (double)(par3 / 16.0F));
+        int var6 = MathHelper.floor_double(periodicTable.posZ);
 
-        if (par1EntityPainting.hangingDirection == 2)
+        if (periodicTable.hangingDirection == 2)
         {
-            i = MathHelper.floor_double(par1EntityPainting.posX + (double)(par2 / 16.0F));
+            var4 = MathHelper.floor_double(periodicTable.posX + (double)(par2 / 16.0F));
         }
 
-        if (par1EntityPainting.hangingDirection == 1)
+        if (periodicTable.hangingDirection == 1)
         {
-            k = MathHelper.floor_double(par1EntityPainting.posZ - (double)(par2 / 16.0F));
+            var6 = MathHelper.floor_double(periodicTable.posZ - (double)(par2 / 16.0F));
         }
 
-        if (par1EntityPainting.hangingDirection == 0)
+        if (periodicTable.hangingDirection == 0)
         {
-            i = MathHelper.floor_double(par1EntityPainting.posX - (double)(par2 / 16.0F));
+            var4 = MathHelper.floor_double(periodicTable.posX - (double)(par2 / 16.0F));
         }
 
-        if (par1EntityPainting.hangingDirection == 3)
+        if (periodicTable.hangingDirection == 3)
         {
-            k = MathHelper.floor_double(par1EntityPainting.posZ + (double)(par2 / 16.0F));
+            var6 = MathHelper.floor_double(periodicTable.posZ + (double)(par2 / 16.0F));
         }
 
-        int l = this.renderManager.worldObj.getLightBrightnessForSkyBlocks(i, j, k, 0);
-        int i1 = l % 65536;
-        int j1 = l / 65536;
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)i1, (float)j1);
+        int var7 = this.renderManager.worldObj.getLightBrightnessForSkyBlocks(var4, var5, var6, 0);
+        int var8 = var7 % 65536;
+        int var9 = var7 / 65536;
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)var8, (float)var9);
         GL11.glColor3f(1.0F, 1.0F, 1.0F);
-    }
+	}
 
-    /**
-     * Actually renders the given argument. This is a synthetic bridge method, always casting down its argument and then
-     * handing it off to a worker function which does the actual work. In all probabilty, the class Render is generic
-     * (Render<T extends Entity) and this method has signature public void doRender(T entity, double d, double d1,
-     * double d2, float f, float f1). But JAD is pre 1.5 so doesn't do that.
-     */
-    public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9)
-    {
-        this.renderThePainting((EntityTableOfElements)par1Entity, par2, par4, par6, par8, par9);
-    }
 }
--- src/minecraft/ljdp/minechem/common/entity/EntityTableOfElements.java
+++ src/minecraft/ljdp/minechem/common/entity/EntityTableOfElements.java
@@ -1,259 +1,67 @@
 package ljdp.minechem.common.entity;
 
-import java.util.List;
-
 import ljdp.minechem.common.MinechemItems;
-
-import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityHanging;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.MathHelper;
+import net.minecraft.util.EnumArt;
 import net.minecraft.world.World;
 
 public class EntityTableOfElements extends EntityHanging {
 	
-	private int tickCounter1;
-    public int direction;
-    public int xPosition;
-    public int yPosition;
-    public int zPosition;
-    public static int tableSizeX = 2048/16;
-    public static int tableSizeY = 1024/16;
+	public EnumArt art;
 	
-	public EntityTableOfElements(World world) {
+	public EntityTableOfElements(World world, int x, int y, int z, int orientation) {
 		super(world);
-        tickCounter1 = 0;
-        direction = 0;
-        yOffset = 0.0F;
-        setSize(0.5F, 0.5F);
+		this.xPosition = x;
+		this.yPosition = y;
+		this.zPosition = z;
+		this.hangingDirection = orientation;
+		this.art = EnumArt.Aztec;
 	}
-	
-	public EntityTableOfElements(World world, int i, int j, int k, int l)
-    {
-        this(world);
-        xPosition = i;
-        yPosition = j;
-        zPosition = k;
-        setFacing(l);
-    }
-	
-	public void setFacing(int i)
-    {
-        direction = i;
-        prevRotationYaw = rotationYaw = i * 90;
-        float f = tableSizeX;
-        float f1 = tableSizeY;
-        float f2 = tableSizeX;
-        if (i == 0 || i == 2)
-        {
-            f2 = 0.5F;
-        }
-        else
-        {
-            f = 0.5F;
-        }
-        f /= 32F;
-        f1 /= 32F;
-        f2 /= 32F;
-        float f3 = (float)xPosition + 0.5F;
-        float f4 = (float)yPosition + 0.5F;
-        float f5 = (float)zPosition + 0.5F;
-        float f6 = 0.5625F;
-        //float f6 = 0.0F;
-        if (i == 0)
-        {
-            f5 -= f6;
-        }
-        if (i == 1)
-        {
-            f3 -= f6;
-        }
-        if (i == 2)
-        {
-            f5 += f6;
-        }
-        if (i == 3)
-        {
-            f3 += f6;
-        }
-        if (i == 0)
-        {
-            f3 -= func_411_c(tableSizeX);
-        }
-        if (i == 1)
-        {
-            f5 += func_411_c(tableSizeX);
-        }
-        if (i == 2)
-        {
-            f3 += func_411_c(tableSizeX);
-        }
-        if (i == 3)
-        {
-            f5 -= func_411_c(tableSizeX);
-        }
-        f4 += func_411_c(tableSizeY);
-        setPosition(f3, f4, f5);
-        float f7 = -0.00625F;
-        boundingBox.setBounds(f3 - f - f7, f4 - f1 - f7, f5 - f2 - f7, f3 + f + f7, f4 + f1 + f7, f5 + f2 + f7);
-    }
-	
-	private float func_411_c(int i)
-    {
-        if (i == 32)
-        {
-            return 0.5F;
-        }
-        return i != 64 ? 0.0F : 0.0F;
-    }
-	
-	public boolean canBeCollidedWith()
-    {
-        return true;
-    }
-	
-	public boolean attackEntityFrom(DamageSource damagesource, int i)
-    {
-        if (!isDead && !worldObj.isRemote)
-        {
-            setDead();
-            setBeenAttacked();
-            worldObj.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ, new ItemStack(MinechemItems.tableelements)));
-        }
-        return true;
-    }
-	
-	public void onUpdate()
-    {
-        if (tickCounter1++ == 100 && !worldObj.isRemote)
-        {
-            tickCounter1 = 0;
-            if (!canStay())
-            {
-                setDead();
-                worldObj.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ, new ItemStack(MinechemItems.tableelements)));
-            }
-        }
-    }
-	
-	public boolean canStay()
-    {
-        if (worldObj.getCollidingBoundingBoxes(this, boundingBox).size() > 0)
-        {
-            return false;
-        }
-        int i = tableSizeX / 16;
-        int j = tableSizeY / 16;
-        int k = xPosition;
-        int l = yPosition;
-        int i1 = zPosition;
-        if (direction == 0)
-        {
-            k = MathHelper.floor_double(posX - (double)((float)tableSizeX / 32F));
-        }
-        if (direction == 1)
-        {
-            i1 = MathHelper.floor_double(posZ - (double)((float)tableSizeX / 32F));
-        }
-        if (direction == 2)
-        {
-            k = MathHelper.floor_double(posX - (double)((float)tableSizeX / 32F));
-        }
-        if (direction == 3)
-        {
-            i1 = MathHelper.floor_double(posZ - (double)((float)tableSizeX / 32F));
-        }
-        l = MathHelper.floor_double(posY - (double)((float)tableSizeY / 32F));
-        for (int j1 = 0; j1 < i; j1++)
-        {
-            for (int k1 = 0; k1 < j; k1++)
-            {
-                Material material;
-                if (direction == 0 || direction == 2)
-                {
-                    material = worldObj.getBlockMaterial(k + j1, l + k1, zPosition);
-                }
-                else
-                {
-                    material = worldObj.getBlockMaterial(xPosition, l + k1, i1 + j1);
-                }
-                if (!material.isSolid())
-                {
-                    return false;
-                }
-            }
-        }
-
-        List list = worldObj.getEntitiesWithinAABBExcludingEntity(this, boundingBox);
-        for (int l1 = 0; l1 < list.size(); l1++)
-        {
-            if (list.get(l1) instanceof EntityTableOfElements)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-	
-	public void moveEntity(double d, double d1, double d2)
-    {
-        if (!worldObj.isRemote && d * d + d1 * d1 + d2 * d2 > 0.0D)
-        {
-            setDead();
-            worldObj.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ, new ItemStack(MinechemItems.tableelements)));
-        }
-    }
-
-    public void addVelocity(double d, double d1, double d2)
-    {
-        if (!worldObj.isRemote && d * d + d1 * d1 + d2 * d2 > 0.0D)
-        {
-            setDead();
-            worldObj.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ, new ItemStack(MinechemItems.tableelements)));
-        }
-    }
 
 	@Override
 	protected void entityInit() {
+		// TODO Auto-generated method stub
+
 	}
 
-	@Override
-	public void readEntityFromNBT(NBTTagCompound nbttagcompound) {
-		direction = nbttagcompound.getByte("Dir");
-        xPosition = nbttagcompound.getInteger("TileX");
-        yPosition = nbttagcompound.getInteger("TileY");
-        zPosition = nbttagcompound.getInteger("TileZ");
-        setFacing(direction);
+	public boolean canStay() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	public int getSizeX() {
+		return this.art.sizeX;
+	}
+	
+	public int getSizeY() {
+		return this.art.sizeY;
 	}
 
-	@Override
-	public void writeEntityToNBT(NBTTagCompound nbttagcompound) {
-		nbttagcompound.setByte("Dir", (byte)direction);
-        nbttagcompound.setInteger("TileX", xPosition);
-        nbttagcompound.setInteger("TileY", yPosition);
-        nbttagcompound.setInteger("TileZ", zPosition);
-	}
-
+	/**
+	 * Function: Get Art Size X.
+	 * @return
+	 */
 	@Override
 	public int func_82329_d() {
-		// TODO Auto-generated method stub
-		return tableSizeX;
+		return getSizeX();
 	}
-
+	
+	/**
+	 * Function: Get Art Size Y.
+	 * @return
+	 */
 	@Override
 	public int func_82330_g() {
-		// TODO Auto-generated method stub
-		return tableSizeY;
+		return getSizeY();
 	}
 
 	@Override
 	public void dropItemStack() {
-		// TODO Auto-generated method stub
-		
+		this.entityDropItem(new ItemStack(MinechemItems.tableelements), 0.0F);
 	}
 
 }
