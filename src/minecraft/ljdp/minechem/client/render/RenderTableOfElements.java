f86f3c45a492b6591e859096a9700037985932f9
Rushmead <Rushmead@techie.com>
2013-04-14 01:11:38 -0700

    Really? Final Commit Right?

--- src/minecraft/ljdp/minechem/client/render/RenderTableOfElements.java
+++ src/minecraft/ljdp/minechem/client/render/RenderTableOfElements.java
@@ -166,6 +166,7 @@ public class RenderTableOfElements extends Render
      * (Render<T extends Entity) and this method has signature public void doRender(T entity, double d, double d1,
      * double d2, float f, float f1). But JAD is pre 1.5 so doesn't do that.
      */
+    @Override
     public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9)
     {
         this.renderThePainting((EntityTableOfElements)par1Entity, par2, par4, par6, par8, par9);
