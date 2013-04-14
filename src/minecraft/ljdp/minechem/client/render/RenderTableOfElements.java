32b19fc1ab79d2ad0fe57f70cf5e41067f1b834d
Rushmead <Rushmead@techie.com>
2013-04-14 01:26:03 -0700

    3rd Update OMG!

--- src/minecraft/ljdp/minechem/client/render/RenderTableOfElements.java
+++ src/minecraft/ljdp/minechem/client/render/RenderTableOfElements.java
@@ -17,19 +17,18 @@ import org.lwjgl.opengl.GL12;
 @SideOnly(Side.CLIENT)
 public class RenderTableOfElements extends Render
 {
-
-    public void renderThePainting(EntityTableOfElements entityTable, double par2, double par4, double par6, float par8, float par9)
+    public void renderThePainting(EntityTableOfElements par1EntityPainting, double par2, double par4, double par6, float par8, float par9)
     {
-    	 GL11.glPushMatrix();
-         GL11.glTranslatef((float)par2, (float)par4, (float)par6);
-         GL11.glRotatef(par8, 0.0F, 1.0F, 0.0F);
-         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-         this.loadTexture(ConstantValue.table_HEX);
-         float f2 = 0.0625F;
-         GL11.glScalef(f2, f2, f2);
-         this.func_77010_a(entityTable, entityTable.tableSizeX, entityTable.tableSizeY, 0, 0);
-         GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-         GL11.glPopMatrix();
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float)par2, (float)par4, (float)par6);
+        GL11.glRotatef(par8, 0.0F, 1.0F, 0.0F);
+        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+        this.loadTexture(ConstantValue.table_HEX);
+        float f2 = 0.0625F;
+        GL11.glScalef(f2, f2, f2);
+        this.func_77010_a(par1EntityPainting, par1EntityPainting.tableSizeX, par1EntityPainting.tableSizeY, 0, 0);
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+        GL11.glPopMatrix();
     }
 
     private void func_77010_a(EntityTableOfElements par1EntityPainting, int par2, int par3, int par4, int par5)
