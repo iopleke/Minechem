8879d7236df7d2e757c716eaff0b67c8eb3c643b
Mfernflower <machacker9000@gmail.com>
2013-06-06 17:03:54 -0700

    FUCKING LEMONS UP MY ASS!

--- src/minecraft/biomesoplenty/api/Blocks.java
+++ src/minecraft/biomesoplenty/api/Blocks.java
@@ -8,7 +8,7 @@ import net.minecraft.block.BlockHalfSlab;
 
 import com.google.common.base.Optional;
 
-public class BOPBlocks
+public class Blocks
 {
 	// Worldgen Blocks
 	public static Optional<? extends Block> ash                         = Optional.absent();
