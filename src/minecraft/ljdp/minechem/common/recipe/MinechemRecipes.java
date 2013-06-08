4a915ddea8066ada6b450a14e1d60f971b6dd78c
Mfernflower <machacker9000@gmail.com>
2013-06-07 17:12:43 -0700

    FUCK YOU JENKINS!!!!!!!

--- src/minecraft/ljdp/minechem/common/recipe/MinechemRecipes.java
+++ src/minecraft/ljdp/minechem/common/recipe/MinechemRecipes.java
@@ -470,17 +470,17 @@ public class MinechemRecipes {
         //
         // BOP CHEMICAL PROCESSING SECTION
         // BlockReferences.getBlockItemStack("darkLog")
-		ItemStack Algae = new BlockReferences.getBlockItemStack("algae");
+		Algae = new BlockReferences.getBlockItemStack("algae");
 		DecomposerRecipe.add(new DecomposerRecipeChance(Algae, 0.1F, new Chemical[] { new Molecule(EnumMolecule.nod) }));
-		ItemStack IndigoCap = new BlockReferences.getBlockItemStack("bluemilk"); // THE BLUE ONES FUCK YOU UP BAD!
+		IndigoCap = new BlockReferences.getBlockItemStack("bluemilk"); // THE BLUE ONES FUCK YOU UP BAD!
 		DecomposerRecipe.add(new DecomposerRecipe(Indigocap, new Chemical[] { new Molecule(EnumMolecule.pantherine), new Molecule(EnumMolecule.psilocybin), new Molecule(EnumMolecule.blueorgodye)}));
-        ItemStack MagicShroom = new BlockReferences.getBlockItemStack("toadstool");
+        MagicShroom = new BlockReferences.getBlockItemStack("toadstool");
 		DecomposerRecipe.add(new DecomposerRecipeChance(MagicShroom, 0.4F, new Chemical[] { new Molecule(EnumMolecule.psilocybin) }));
-		ItemStack Willowasprin = new BlockReferences.getBlockItemStack("willowLog");
+		Willowasprin = new BlockReferences.getBlockItemStack("willowLog");
 		DecomposerRecipe.add(new DecomposerRecipeChance(Willowasprin, 0.4F, new Chemical[] { new Molecule(EnumMolecule.asprin, 2) }));
-		ItemStack FoxFire = new BlockReferences.getBlockItemStack("glowshroom");
+		FoxFire = new BlockReferences.getBlockItemStack("glowshroom");
 		DecomposerRecipe.add(new DecomposerRecipeChance(FoxFire, 0.8F, new Chemical[] { new Molecule(EnumMolecule.psilocybin, 2), new Element(EnumElement.P, 4) }));
-	    ItemStack Daisy1 = new BlockReferences.getBlockItemStack("daisy");
+	    Daisy1 = new BlockReferences.getBlockItemStack("daisy");
         DecomposerRecipe.add(new DecomposerRecipeChance(Daisy1, 0.3F, new Chemical[] { new Molecule(EnumMolecule.shikimicAcid, 2), new Molecule(EnumMolecule.water, 2) }));
 		// END 
         this.addDecomposerRecipesFromMolecules();
