16e0df5d396f157b3a55ce11800c2b385ce52271
Mfernflower <machacker9000@gmail.com>
2013-06-16 17:56:16 -0400

    Fix

--- src/minecraft/ljdp/minechem/common/recipe/MinechemRecipes.java
+++ src/minecraft/ljdp/minechem/common/recipe/MinechemRecipes.java
@@ -486,7 +486,7 @@ public class MinechemRecipes {
         ItemStack Willowasprin = BlockReferences.getBlockItemStack("willowLog");
         DecomposerRecipe.add(new DecomposerRecipeChance(Willowasprin, 0.4F, new Chemical[] { new Molecule(EnumMolecule.asprin, 2) }));
         ItemStack FoxFire = BlockReferences.getBlockItemStack("glowshroom");
-        DecomposerRecipe.add(new DecomposerRecipeChance(FoxFire, 0.8F, new Chemical[] { new Molecule(EnumMolecule.psilocybin, 2), new Element(EnumElement.P, 4) }));
+        DecomposerRecipe.add(new DecomposerRecipeChance(FoxFire, 0.6F, new Chemical[] { new Molecule(EnumMolecule.psilocybin, 2), new Element(EnumElement.P, 2) }));
         ItemStack Daisy1 = BlockReferences.getBlockItemStack("daisy");
         DecomposerRecipe.add(new DecomposerRecipeChance(Daisy1, 0.3F, new Chemical[] { new Molecule(EnumMolecule.shikimicAcid, 2), new Molecule(EnumMolecule.water, 2) }));
 		ItemStack WitherFlower = BlockReferences.getBlockItemStack("deathbloom");
