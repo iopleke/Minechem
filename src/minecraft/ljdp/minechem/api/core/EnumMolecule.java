b1517d3932252c128c1e87b09eab1c4b503f8edf
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-07-12 00:49:23 -0600

    Added Potasium Nitrate Coloring

--- src/minecraft/ljdp/minechem/api/core/EnumMolecule.java
+++ src/minecraft/ljdp/minechem/api/core/EnumMolecule.java
@@ -16,7 +16,7 @@ public enum EnumMolecule {
     carbonDioxide(2, "Carbon Dioxide", 0.5F, 0.5F, 0.5F, 0.25F, 0.25F, 0.25F, new Element(C), new Element(O, 2)),
     nitrogenDioxide(3, "Nitrogen Dioxide", 1, 0.65F, 0, 0.5F, 0.1412F, 0.1843F, new Element(N), new Element(O, 2)),
     toluene(4, "Toluene", 1, 1, 1, 0.8F, 0.8F, 0.8F, new Element(C, 7), new Element(H, 8)),
-    potassiumNitrate(5, "Potassium Nitrate", new Element(K), new Element(N), new Element(O, 3)),
+    potassiumNitrate(5, "Potassium Nitrate", 0.9F, 0.9F, 0.9F, 0.8F, 0.8F, 0.8F, new Element(K), new Element(N), new Element(O, 3)),
     tnt(6, "Trinitrotoluene", new Element(C, 6), new Element(H, 2), new Molecule(nitrogenDioxide, 3), new Molecule(toluene)),
     siliconDioxide(7, "Silicon Dioxide", new Element(Si), new Element(O, 2)),
     calcite(8, "Calcite", new Element(Ca), new Element(C), new Element(O, 3)),
