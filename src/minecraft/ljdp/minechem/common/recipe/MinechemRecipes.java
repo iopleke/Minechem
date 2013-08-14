b984f7d2d7739500bda61c96fe92e2c32afb2928
Mfernflower <machacker9000@gmail.com>
2013-08-13 22:01:38 -0400

    Fix Crafting

--- src/minecraft/ljdp/minechem/common/recipe/MinechemRecipes.java
+++ src/minecraft/ljdp/minechem/common/recipe/MinechemRecipes.java
@@ -84,7 +84,7 @@ public class MinechemRecipes {
         GameRegistry.addRecipe(new ItemStack(MinechemItems.IAintAvinit), new Object[] { "ZZZ", "ZSZ", " S ", Character.valueOf('Z'), new ItemStack(Item.ingotIron), Character.valueOf('S'), new ItemStack(Item.stick) });
         // GameRegistry.addRecipe(new ItemStack(MinechemItems.blueprint, 1, 1), new Object[] { "ZZZ", "SSS", "ZZZ", Character.valueOf('Z'), paper, Character.valueOf('S'), bdye });
         GameRegistry.addShapelessRecipe(new ItemStack(MinechemItems.journal), new Object[] { new ItemStack(Item.book), new ItemStack(MinechemItems.testTube) });
-        GameRegistry.addShapelessRecipe(new ItemStack(MinechemItems.EmptyPillz,4), new Object[] { "CZC", Character.valueOf('C'), new ItemStack(Item.slimeBall), Character.valueOf('Z'), new ItemStack(Item.sugar) });
+        GameRegistry.addShapelessRecipe(new ItemStack(MinechemItems.EmptyPillz,4), new Object[] { new ItemStack(Item.slimeBall), new ItemStack(Item.sugar) });
 		for (EnumElement element : EnumElement.values()) {
             GameRegistry.addShapelessRecipe(new ItemStack(MinechemItems.testTube), new Object[] { new ItemStack(MinechemItems.element, element.ordinal()) });
         }
