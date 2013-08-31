af5139e8bac2997ead790629b50d5c59c41bad34
pixlepix <adlytempleton@gmail.com>
2013-08-30 21:11:00 -0400

    Fixed Adjacent Chemical Storage Glitching Up

--- src/minecraft/ljdp/minechem/client/render/tileentity/TileEntityChemicalStorageRenderer.java
+++ src/minecraft/ljdp/minechem/client/render/tileentity/TileEntityChemicalStorageRenderer.java
@@ -37,14 +37,14 @@ public class TileEntityChemicalStorageRenderer extends TileEntitySpecialRenderer
             var9 = chemicalStorage.getBlockMetadata();
 
             if (var10 != null && var9 == 0) {
-                ((BlockChest) var10).unifyAdjacentChests(chemicalStorage.getWorldObj(), chemicalStorage.xCoord, chemicalStorage.yCoord, chemicalStorage.zCoord);
+                //((BlockChest) var10).unifyAdjacentChests(chemicalStorage.getWorldObj(), chemicalStorage.xCoord, chemicalStorage.yCoord, chemicalStorage.zCoord);
                 var9 = chemicalStorage.getBlockMetadata();
             }
 
-            chemicalStorage.checkForAdjacentChests();
+            //chemicalStorage.checkForAdjacentChests();
         }
 
-        if (chemicalStorage.adjacentChestZNeg == null && chemicalStorage.adjacentChestXNeg == null) {
+        //if (chemicalStorage.adjacentChestZNeg == null && chemicalStorage.adjacentChestXNeg == null) {
             ModelChest var14 = this.chestModel;
 
             func_110628_a(new ResourceLocation(ConstantValue.MOD_ID,ConstantValue.CHEMICAL_STORAGE_MODEL));
@@ -111,7 +111,7 @@ public class TileEntityChemicalStorageRenderer extends TileEntitySpecialRenderer
             GL11.glDisable(GL12.GL_RESCALE_NORMAL);
             GL11.glPopMatrix();
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        }
+       // }
     }
 
     public void renderTileEntityAt(TileEntity par1TileEntity, double par2, double par4, double par6, float par8) {
