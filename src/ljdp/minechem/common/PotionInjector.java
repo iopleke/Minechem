f27078949b4230e776a37595f3acbeb3291b7266
Mandrake Naader Fernflower <machacker9000@gmail.com>
2013-09-15 21:33:21 -0400

    Create PotionInjector.java

---
+++ src/ljdp/minechem/common/PotionInjector.java
@@ -0,0 +1,39 @@
+package ljdp.minechem.common;
+
+// This code was based off of https://github.com/Glitchfiend/BiomesOPlenty/blob/master/common/biomesoplenty/configuration/BOPPotions.java
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import net.minecraft.potion.Potion;
+
+public class PotionInjector {
+	public static Potion atropineHigh;
+
+	public static void inject() {
+		int potionTotal = Potion.potionTypes.length;
+		Potion[] effectAray = new Potion[potionTotal + 2];
+		System.arraycopy(Potion.potionTypes, 0, effectAray, 0, potionTotal);
+		Field field = null;
+		Field[] fields = Potion.class.getDeclaredFields();
+
+		for (Field f : fields)
+			if (f.getName().equals("potionTypes")
+					|| f.getName().equals("field_76425_a")) {
+				field = f;
+				break;
+			}
+
+		try {
+			field.setAccessible(true);
+
+			Field modfield = Field.class.getDeclaredField("modifiers");
+			modfield.setAccessible(true);
+			modfield.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+			field.set(null, effectAray);
+		} catch (Exception e) {
+			System.err.println("He's Dead Jim" + " " + e);
+		}
+		atropineHigh = new PotionProvider(potionTotal, true, 0x00FF6E).setPotionName("Delirium").setIconIndex(2, 1); // That icon is a refrence to alice in wonderland :P
+	}
+}
