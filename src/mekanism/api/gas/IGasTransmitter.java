68905b749056add9315bf7ca99d82de9bc996f09
pixlepix <adlytempleton@gmail.com>
2013-09-19 19:42:45 -0400

    Removed mekanism API. Should fix CME under certain circumstances

--- src/mekanism/api/gas/IGasTransmitter.java
+++
@@ -1,9 +0,0 @@
-package mekanism.api.gas;
-
-import mekanism.api.transmitters.ITransmitter;
-import net.minecraft.tileentity.TileEntity;
-
-public interface IGasTransmitter extends ITransmitter<GasNetwork>
-{
-    public boolean canTransferGasToTube(TileEntity tile);
-}
