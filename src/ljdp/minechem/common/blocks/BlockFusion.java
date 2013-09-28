9ce185a40abc0007b3cc1d512d2360fb3f5ebfaf
pixlepix <adlytempleton@gmail.com>
2013-09-28 09:17:44 -0400

    Basic work on Fission Tile Entity

--- src/ljdp/minechem/common/blocks/BlockFusion.java
+++ src/ljdp/minechem/common/blocks/BlockFusion.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ljdp.minechem.common.ModMinechem;
+import ljdp.minechem.common.tileentity.TileEntityFission;
 import ljdp.minechem.common.tileentity.TileEntityFusion;
 import ljdp.minechem.common.tileentity.TileEntityProxy;
 import ljdp.minechem.common.utils.ConstantValue;
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
