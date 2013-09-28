7143c44d13ae957805d01dbe6bbc45fab2bccedb
pixlepix <adlytempleton@gmail.com>
2013-09-28 17:42:04 -0400

    Removed debug messages and actually added uranium

---
+++ src/ljdp/minechem/common/MinechemGeneration.java
@@ -0,0 +1,27 @@
+package ljdp.minechem.common;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class MinechemGeneration implements IWorldGenerator {
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world,
+			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		if(world.provider.dimensionId==0){
+			for(int k=0;k<2;k++){
+				int firstBlockXCoord = chunkX + random.nextInt(16);
+				int firstBlockYCoord = random.nextInt(30);
+				int firstBlockZCoord = chunkZ + random.nextInt(16);
+				WorldGenMinable mineable=new WorldGenMinable(MinechemBlocks.uranium.blockID, 4);
+				mineable.generate(world, random, firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
+			}
+		}
+		
+	}
+
+}
