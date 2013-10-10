08fbd4786c91f49b55f0caded1e519245d218fa5
diadistis <bsotirid@gmail.com>
2013-10-10 03:45:14 +0300

    Fixed ore name checks for Underground Biomes

--- src/ljdp/minechem/common/recipe/handlers/UndergroundBiomesOreDictionaryHandler.java
+++ src/ljdp/minechem/common/recipe/handlers/UndergroundBiomesOreDictionaryHandler.java
@@ -1,11 +1,13 @@
 package ljdp.minechem.common.recipe.handlers;
 
 import java.util.Arrays;
+import java.util.logging.Level;
 
 import ljdp.minechem.api.core.Chemical;
 import ljdp.minechem.api.core.EnumMolecule;
 import ljdp.minechem.api.core.Molecule;
 import ljdp.minechem.api.recipe.DecomposerRecipe;
+import ljdp.minechem.common.ModMinechem;
 import ljdp.minechem.common.recipe.OreDictionaryHandler;
 import net.minecraftforge.oredict.OreDictionary.OreRegisterEvent;
 
@@ -41,8 +43,11 @@ public class UndergroundBiomesOreDictionaryHandler implements
 
 	@Override
 	public boolean canHandle(OreRegisterEvent event) {
-		if (Arrays.asList(EnumOre.values()).contains(event.Name))
-			return true;
+		for (EnumOre ore : EnumOre.values()) {
+	        if (ore.name().equals(event.Name)) {
+	            return true;
+	        }
+	    }
 		return false;
 	}
 
