76a53d93c8bf407329169077dfad4f5635ec2d47
pixlepix <adlytempleton@gmail.com>
2013-11-04 20:17:17 -0500

    Revert "Made build file better"
    This reverts commit 747cfbe8b174bf97d6bc695b93c6336873d68ef5.

--- src/ljdp/minechem/client/render/tileentity/TileEntityGhostBlockRenderer.java
+++ src/ljdp/minechem/client/render/tileentity/TileEntityGhostBlockRenderer.java
@@ -6,12 +6,12 @@ import java.util.Random;
 import ljdp.minechem.common.blueprint.BlueprintBlock;
 import ljdp.minechem.common.blueprint.MinechemBlueprint;
 import ljdp.minechem.common.tileentity.TileEntityGhostBlock;
-import net.minecraft.block.Block;
+
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.renderer.RenderBlocks;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.World;
 
 public class TileEntityGhostBlockRenderer extends TileEntitySpecialRenderer {
 
@@ -21,17 +21,16 @@ public class TileEntityGhostBlockRenderer extends TileEntitySpecialRenderer {
     @Override
     public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float scale) {
         BlueprintBlock blueprintBlock = getBlueprintBlock(tileEntity);
-        if (blueprintBlock == null){
+        if (blueprintBlock == null)
             return;
-        }
-        /*float alpha = random.nextFloat();
+        float alpha = random.nextFloat();
 
         GL11.glPushMatrix();
         GL11.glTranslated(x + 0.5D, y + 0.5D, z + 0.5D);
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glEnable(GL11.GL_BLEND);
         // GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_DST_COLOR);
-        //GL11.glBlendFunc(GL11.GL_SRC_ALPHA, 1-GL11.GL_DST_ALPHA);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, 1-GL11.GL_DST_ALPHA);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         renderBlocks.blockAccess = tileEntity.worldObj;
         renderBlocks.renderBlockAsItem(blueprintBlock.block, blueprintBlock.metadata, alpha);
@@ -39,33 +38,6 @@ public class TileEntityGhostBlockRenderer extends TileEntitySpecialRenderer {
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glPopMatrix();
-        */
-
-        Tessellator tessellator = Tessellator.instance;
-        tessellator.startDrawingQuads();
-        Block block = blueprintBlock.block;
-        World world=tileEntity.worldObj;
-        System.out.println(block);
-        renderBlocks.renderMaxX = 1;
-        renderBlocks.renderMinX = 0;
-        renderBlocks.renderMaxY = 1;
-        renderBlocks.renderMinY = 0;
-        renderBlocks.renderMaxZ =1;
-        renderBlocks.renderMinZ = 0;
-            renderBlocks.renderFaceYNeg(block, 0,0,0, block.getIcon(0, blueprintBlock.metadata));
-        
-            renderBlocks.renderFaceYPos(block, 0,0,0, block.getIcon(1, blueprintBlock.metadata));
-        
-            renderBlocks.renderFaceZNeg(block, 0,0,0, block.getIcon(2, blueprintBlock.metadata));
-       
-            renderBlocks.renderFaceZPos(block, 0,0,0, block.getIcon(3, blueprintBlock.metadata));
-            renderBlocks.renderFaceXNeg(block, 0,0,0, block.getIcon(4, blueprintBlock.metadata));
-        
-        
-            renderBlocks.renderFaceXPos(block, 0,0,0, block.getIcon(5, blueprintBlock.metadata));
-        
-        tessellator.draw();
-
     }
     private BlueprintBlock getBlueprintBlock(TileEntity tileEntity) {
         if (tileEntity instanceof TileEntityGhostBlock) {
