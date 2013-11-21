61b232d29016c4f9c6ca9c5f8f74a8e14548ce7d
Mandrake Naader Fernflower <machacker9000@gmail.com>
2013-11-21 11:05:14 -0500

    Start adding more mod compat

---
+++ src/ljdp/minechem/common/recipe/handlers/TiCOreDictionaryHandler.java
@@ -0,0 +1,20 @@
+package ljdp.minechem.common.recipe.handlers;
+
+import net.minecraftforge.oredict.OreDictionary.OreRegisterEvent;
+import ljdp.minechem.common.recipe.OreDictionaryHandler;
+
+public class TiCOreDictionaryHandler implements OreDictionaryHandler {
+
+	@Override
+	public boolean canHandle(OreRegisterEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void handle(OreRegisterEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
