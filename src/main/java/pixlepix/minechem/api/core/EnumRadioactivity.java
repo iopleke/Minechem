887765f474fc5f2ec28f6da87511dde39e5f931d
pixlepix <adlytempleton@gmail.com>
2014-01-29 05:54:21 -0500

    Increased radioactivity duration

--- src/main/java/pixlepix/minechem/api/core/EnumRadioactivity.java
+++ src/main/java/pixlepix/minechem/api/core/EnumRadioactivity.java
@@ -5,12 +5,12 @@ import pixlepix.minechem.api.util.Constants;
 public enum EnumRadioactivity {
     stable(0, 0),
     hardlyRadioactive(Constants.TICKS_PER_DAY, 1),
-    slightlyRadioactive(Constants.TICKS_PER_HOUR, 2),
-    radioactive(Constants.TICKS_PER_MINUTE * 10, 4),
-    highlyRadioactive(Constants.TICKS_PER_MINUTE * 2, 8),
-    extremelyRadioactive(Constants.TICKS_PER_SECOND * 20, 16);
+	slightlyRadioactive(Constants.TICKS_PER_HOUR * 12, 2),
+	radioactive(Constants.TICKS_PER_HOUR * 6, 6),
+	highlyRadioactive(Constants.TICKS_PER_HOUR * 3, 8),
+	extremelyRadioactive(Constants.TICKS_PER_HOUR * 1, 16);
 
-    private int life;
+	private int life;
     private int damage;
 
     private EnumRadioactivity(int life, int damage) {
