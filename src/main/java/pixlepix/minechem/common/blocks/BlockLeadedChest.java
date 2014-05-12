273040454639fa79d1178c84ff2d769d51d4c5cf
jakimfett <jakimfett@picraft.com>
2014-05-12 13:54:30 -0700

    Fix block placement direction based on player location

--- src/main/java/pixlepix/minechem/common/blocks/BlockLeadedChest.java
+++ src/main/java/pixlepix/minechem/common/blocks/BlockLeadedChest.java
@@ -3,12 +3,14 @@ package pixlepix.minechem.common.blocks;
 import java.util.Random;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import pixlepix.minechem.common.ModMinechem;
 import pixlepix.minechem.common.tileentity.TileEntityLeadedChest;
@@ -94,5 +96,28 @@ public class BlockLeadedChest extends BlockContainer {
     @Override
     public boolean renderAsNormalBlock(){
         return false;
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase el, ItemStack is) {
+        byte facing = 0;
+        int facingI = MathHelper.floor_double((double) (el.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (facingI == 0) {
+            facing = 2;
+        }
+
+        if (facingI == 1) {
+            facing = 5;
+        }
+
+        if (facingI == 2) {
+            facing = 3;
+        }
+
+        if (facingI == 3) {
+            facing = 4;
+        }
+        world.setBlockMetadataWithNotify(x, y, z, facing, 2);
     }
 }
