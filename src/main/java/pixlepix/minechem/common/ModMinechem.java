75b21305d055c6d4cd774fc56ac82c72e7282bd6
Maxwolf Goodliffe <devs@madsciencemod.com>
2014-05-15 02:39:59 -0700

    Made ID, ChannelName and Name strings static references in main ModMinechem to remove hard-coded references.

--- src/main/java/pixlepix/minechem/common/ModMinechem.java
+++ src/main/java/pixlepix/minechem/common/ModMinechem.java
@@ -2,6 +2,7 @@ package pixlepix.minechem.common;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -13,7 +14,6 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -28,8 +28,10 @@ import net.minecraftforge.common.ChestGenHooks;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
+
 import org.modstats.ModstatInfo;
 import org.modstats.Modstats;
+
 import pixlepix.minechem.client.TickHandler;
 import pixlepix.minechem.client.gui.tabs.*;
 import pixlepix.minechem.common.blueprint.MinechemBlueprint;
@@ -42,152 +44,188 @@ import pixlepix.minechem.common.recipe.MinechemRecipes;
 import pixlepix.minechem.common.utils.ConstantValue;
 import pixlepix.minechem.computercraft.ICCMain;
 import pixlepix.minechem.fluid.FluidHelper;
-import universalelectricity.prefab.TranslationHelper;
 
 import java.util.logging.Logger;
-// import pixlepix.minechem.particlephysics.plugins.BOPModule;
-// import cpw.mods.fml.particlephysics.Loader;
 
-@Mod(modid = "minechem", name = "MineChem", version = "5.0.3")
-@ModstatInfo(prefix = "minechem")
-@NetworkMod(clientSideRequired = true, serverSideRequired = false, channels = { PacketHandler.MINECHEM_PACKET_CHANNEL }, packetHandler = PacketHandler.class)
+@Mod(modid = ModMinechem.ID, name = ModMinechem.NAME, version = "5.0.3", useMetadata = false, acceptedMinecraftVersions = "[1.6.4,)", dependencies = "required-after:Forge@[9.11.1.953,);after:BuildCraft|Energy;after:factorization;after:IC2;after:Railcraft;after:ThermalExpansion")
+@ModstatInfo(prefix = ModMinechem.ID)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false, channels = { ModMinechem.CHANNEL_NAME }, packetHandler = PacketHandler.class)
+public class ModMinechem
+{
+    /** Internal mod name used for reference purposes and resource gathering. **/
+    public static final String ID = "minechem";
+    
+    /** Network name that is used in the NetworkMod. **/
+    public static final String CHANNEL_NAME = ID;
+    
+    /** User friendly version of our mods name. **/
+    public static final String NAME = "MineChem";
+    
+    /** Reference to how many ticks make up a second in Minecraft. **/
+    public static final int SECOND_IN_TICKS = 20;
+    
+    /** Provides logging **/
+    @Instance(value = CHANNEL_NAME)
+    public static ModMinechem INSTANCE;
 
-public class ModMinechem {
-	@Instance("minechem")
-	public static ModMinechem instance;
+    /** Provides standard logging from the Forge. **/
+    public static Logger LOGGER;
 
-	public static Logger logger;
+    @SidedProxy(clientSide = "pixlepix.minechem.client.ClientProxy", serverSide = "pixlepix.minechem.common.CommonProxy")
+    public static CommonProxy PROXY;
+    
+    /** Creative mode tab that shows up in Minecraft. **/
+    public static CreativeTabs minechemTab = new CreativeTabMinechem(ModMinechem.NAME);
+    
+    /** Provides standardized configuration file offered by the Forge. **/
+    private static Configuration CONFIG;
+    
+    /** List of supported languages. **/
+    private static final String[] LANGUAGES_SUPPORTED = new String[] { "en_US", "zh_CN", "de_DE" };
+    
+    public static final ResourceLocation ICON_ENERGY = new ResourceLocation(ModMinechem.ID, ConstantValue.ICON_BASE + "i_power.png");
+    public static final ResourceLocation ICON_FULL_ENERGY = new ResourceLocation(ModMinechem.ID, ConstantValue.ICON_BASE + "i_fullEower.png");
+    public static final ResourceLocation ICON_HELP = new ResourceLocation(ModMinechem.ID, ConstantValue.ICON_BASE + "i_help.png");
+    public static final ResourceLocation ICON_JAMMED = new ResourceLocation(ModMinechem.ID, ConstantValue.ICON_BASE + "i_jammed.png");
+    public static final ResourceLocation ICON_NO_BOTTLES = new ResourceLocation(ModMinechem.ID, ConstantValue.ICON_BASE + "i_noBottles.png");
+    public static final ResourceLocation ICON_NO_RECIPE = new ResourceLocation(ModMinechem.ID, ConstantValue.ICON_BASE + "i_noRecipe.png");
+    public static final ResourceLocation ICON_NO_ENERGY = new ResourceLocation(ModMinechem.ID, ConstantValue.ICON_BASE + "i_unpowered.png");
 
-	public static final ResourceLocation ICON_ENERGY = new ResourceLocation(ConstantValue.MOD_ID, ConstantValue.ICON_BASE + "i_power.png");
-	public static final ResourceLocation ICON_FULL_ENERGY = new ResourceLocation(ConstantValue.MOD_ID, ConstantValue.ICON_BASE + "i_fullEower.png");
-	public static final ResourceLocation ICON_HELP = new ResourceLocation(ConstantValue.MOD_ID, ConstantValue.ICON_BASE + "i_help.png");
-	public static final ResourceLocation ICON_JAMMED = new ResourceLocation(ConstantValue.MOD_ID, ConstantValue.ICON_BASE + "i_jammed.png");
-	public static final ResourceLocation ICON_NO_BOTTLES = new ResourceLocation(ConstantValue.MOD_ID, ConstantValue.ICON_BASE + "i_noBottles.png");
-	public static final ResourceLocation ICON_NO_RECIPE = new ResourceLocation(ConstantValue.MOD_ID, ConstantValue.ICON_BASE + "i_noRecipe.png");
-	public static final ResourceLocation ICON_NO_ENERGY = new ResourceLocation(ConstantValue.MOD_ID, ConstantValue.ICON_BASE + "i_unpowered.png");
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        // Register instance.
+        INSTANCE = this;
+        
+        // Setup logging.
+        LOGGER = event.getModLog();
+        LOGGER.setParent(FMLLog.getLogger());
 
-	@SidedProxy(clientSide = "pixlepix.minechem.client.ClientProxy", serverSide = "pixlepix.minechem.common.CommonProxy")
-	public static CommonProxy proxy;
-	public static CreativeTabs minechemTab = new CreativeTabMinechem("MineChem");
-	private Configuration config;
-        public boolean worldGen = true;
-        public static boolean debug = false;
-	private static final String[] LANGUAGES_SUPPORTED = new String[]{ "en_US", "zh_CN", "de_DE" };
-	public static String GUITABLEID = "2";
+        // Load configuration.
+        LOGGER.info("Loading configuration...");
+        CONFIG = new Configuration(event.getSuggestedConfigurationFile());
+        Settings.load(CONFIG);
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		logger = Logger.getLogger("minechem");
-		logger.setParent(FMLLog.getLogger());
+        // Register items and blocks.
+        LOGGER.info("Registering Items...");
+        MinechemItems.registerItems();
+        
+        LOGGER.info("Registering Blocks...");
+        MinechemBlocks.registerBlocks();
+        
+        LOGGER.info("Registering Blueprints...");
+        MinechemBlueprint.registerBlueprints();
+        
+        LOGGER.info("Registering Recipe Handlers...");
+        MinechemRecipes.getInstance().RegisterHandlers();
+        MinechemRecipes.getInstance().RegisterRecipes();
+        MinechemRecipes.getInstance().registerFluidRecipies();
+        
+        LOGGER.info("Registering OreDict Compatability...");
+        MinechemItems.registerToOreDictionary();
 
-		System.out.println(TranslationHelper.loadLanguages(ConstantValue.LANG_DIR, LANGUAGES_SUPPORTED));
+        LOGGER.info("Registering Minechem Recipes...");
+        MinecraftForge.EVENT_BUS.register(MinechemRecipes.getInstance());
+        
+        LOGGER.info("Registering Chemical Effects...");
+        MinecraftForge.EVENT_BUS.register(new CoatingSubscribe());
+        
+        LOGGER.info("Registering Polytool Event Handler...");
+        MinecraftForge.EVENT_BUS.register(new PolytoolEventHandler());
 
-		loadConfig(event);
-
-		MinechemItems.registerItems();
-
-		MinechemBlocks.registerBlocks();
-		MinechemBlueprint.registerBlueprints();
-		MinechemRecipes.getInstance().RegisterHandlers();
-		MinechemRecipes.getInstance().RegisterRecipes();
-		MinechemItems.registerToOreDictionary();
-
-		proxy.registerHooks();
-
-		MinecraftForge.EVENT_BUS.register(MinechemRecipes.getInstance());
-		MinecraftForge.EVENT_BUS.register(this);
-
-		PotionInjector.inject();
-
-		logger.info("PREINT PASSED");
-
-	}
-
-	@EventHandler
-	public void init(FMLInitializationEvent event) {
-		CraftingManager.getInstance().getRecipeList().add(new CoatingRecipe());
-		FluidHelper.registerFluids();
-		GameRegistry.registerWorldGenerator(new MinechemGeneration());
-		NetworkRegistry.instance().registerGuiHandler(this, new GuiHandler());
-		TickRegistry.registerScheduledTickHandler(new ScheduledTickHandler(), Side.SERVER);
-		proxy.registerRenderers();
-		logger.info("INIT PASSED");
-		LanguageRegistry.instance().addStringLocalization("itemGroup.MineChem", "en_US", "MineChem");
-		Modstats.instance().getReporter().registerMod(this);
-	}
-
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
-		initComputerCraftAddon(event);
-		// initBOP();
-		DoDungeon();
-		
-		//Flexible enchantment location
-		EnchantmentCoated.registerCoatings();
-		MinecraftForge.EVENT_BUS.register(new CoatingSubscribe());
-
-		MinecraftForge.EVENT_BUS.register(new PolytoolEventHandler());
-
-		MinechemRecipes.getInstance().registerFluidRecipies();
-		//Moved to enum ore
-		//DecomposerRecipe.add(new DecomposerRecipe(new ItemStack(MinechemBlocks.uranium), new Chemical[] { new Element(EnumElement.U, 32) }));
-
-		logger.info("POSTINIT PASSED");
-	}
-
-	private void DoDungeon() {
-		ChestGenHooks ChestProvider = ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST);
-		ItemStack A = new ItemStack(MinechemItems.blueprint, 1, 0);
-		ItemStack B = new ItemStack(MinechemItems.blueprint, 1, 1);
-		ChestProvider.addItem(new WeightedRandomChestContent(A, 10, 80, 1));
-		ChestProvider.addItem(new WeightedRandomChestContent(B, 10, 80, 1));
-		logger.info("Injected blueprints into loot chest generator");
-	}
-
-	private void initComputerCraftAddon(FMLPostInitializationEvent event) {
-		Object ccMain = event.buildSoftDependProxy("CCTurtle", "pixlepix.minechem.computercraft.CCMain");
-		if (ccMain != null) {
-			ICCMain iCCMain = (ICCMain) ccMain;
-			iCCMain.loadConfig(config);
-			iCCMain.init();
-			logger.info("ComputerCraft interface loaded");
-		}
-	}
-
-  /*
-  private void initBOP (){
-    if (Loader.isModLoaded("BiomesOPlenty"))
-    { 
-    logger.info("BOP support loaded");
-	System.out.println("MineChem - If for any reason MineChem & Minecraft crashes. \n Try updating BOP \n");
-	BOPModule.DoBopExport();
+        LOGGER.info("PREINT PASSED");
     }
-  }
-*/
 
-	private void loadConfig(FMLPreInitializationEvent event) {
-		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-		MinechemBlocks.loadConfig(config);
-		MinechemItems.loadConfig(config);
-                this.worldGen = config.get("misc", "worldgen", true).getBoolean(true);
-                this.debug = config.get("misc", "debug", false).getBoolean(false);
-		config.save();
-		this.config = config;
-	}
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        LOGGER.info("Registering Proxy Hooks...");
+        PROXY.registerHooks();
+        
+        LOGGER.info("Activating Potion Injector...");
+        PotionInjector.inject();
+        
+        LOGGER.info("Matching Pharmacology Effects to Chemicals...");
+        CraftingManager.getInstance().getRecipeList().add(new CoatingRecipe());
+        
+        LOGGER.info("Registering fluids...");
+        FluidHelper.registerFluids();
+        
+        LOGGER.info("Registering Ore Generation...");
+        GameRegistry.registerWorldGenerator(new MinechemGeneration());
+        
+        LOGGER.info("Registering GUI and Container handlers...");
+        NetworkRegistry.instance().registerGuiHandler(this, new GuiHandler());
+        
+        LOGGER.info("Register Tick Handler for chemical effects tracking...");
+        TickRegistry.registerScheduledTickHandler(new ScheduledTickHandler(), Side.SERVER);
+        
+        LOGGER.info("Registering ClientProxy Rendering Hooks...");
+        PROXY.registerRenderers();
+        
+        LOGGER.info("Registering ModStats Usage Tracking...");
+        Modstats.instance().getReporter().registerMod(this);
+        
+        LOGGER.info("INIT PASSED");
+    }
 
-	//Changed this from TextureStitchEvent.pre
-	@SideOnly(Side.CLIENT)
-	public void textureHook(IconRegister icon) {
-		TabStateControl.unpoweredIcon = icon.registerIcon(ConstantValue.UNPOWERED_ICON);
-		TabStateControlSynthesis.noRecipeIcon = icon.registerIcon(ConstantValue.NO_RECIPE_ICON);
-		TabEnergy.powerIcon = icon.registerIcon(ConstantValue.POWER_ICON);
-		TabHelp.helpIcon = icon.registerIcon(ConstantValue.HELP_ICON);
-		TabTable.helpIcon = icon.registerIcon(ConstantValue.HELP_ICON);
-		TabJournal.helpIcon = icon.registerIcon(ConstantValue.POWER_ICON);
-	}
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        // Activate mod plugins.
+        addonComputerCraft(event);
+        addonDungeonLoot();
 
-	@ForgeSubscribe
-	public void onPreRender(RenderGameOverlayEvent.Pre e) {
-		TickHandler.renderEffects();
-	}
+        LOGGER.info("Activating Chemical Effect Layering (Coatings)...");
+        EnchantmentCoated.registerCoatings();
+
+        LOGGER.info("POSTINIT PASSED");
+    }
+
+    private void addonDungeonLoot()
+    {
+        LOGGER.info("Adding rare chemicals to dungeon loot...");
+        ChestGenHooks ChestProvider = ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST);
+        ItemStack A = new ItemStack(MinechemItems.blueprint, 1, 0);
+        ItemStack B = new ItemStack(MinechemItems.blueprint, 1, 1);
+        ChestProvider.addItem(new WeightedRandomChestContent(A, 10, 80, 1));
+        ChestProvider.addItem(new WeightedRandomChestContent(B, 10, 80, 1));
+    }
+
+    private void addonComputerCraft(FMLPostInitializationEvent event)
+    {
+        if (Loader.isModLoaded("ComputerCraft"))
+        {
+            LOGGER.info("Initilizing ComputerCraft Addon...");
+            Object ccMain = event.buildSoftDependProxy("CCTurtle", "pixlepix.minechem.computercraft.CCMain");
+            if (ccMain != null)
+            {
+                ICCMain iCCMain = (ICCMain) ccMain;
+                iCCMain.loadConfig(CONFIG);
+                iCCMain.init();
+                LOGGER.info("ComputerCraft interface loaded!");
+            }
+            else
+            {
+                LOGGER.warning("Unable to load ComputerCraft interface.");
+            }
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void textureHook(IconRegister icon)
+    {
+        TabStateControl.unpoweredIcon = icon.registerIcon(ConstantValue.UNPOWERED_ICON);
+        TabStateControlSynthesis.noRecipeIcon = icon.registerIcon(ConstantValue.NO_RECIPE_ICON);
+        TabEnergy.powerIcon = icon.registerIcon(ConstantValue.POWER_ICON);
+        TabHelp.helpIcon = icon.registerIcon(ConstantValue.HELP_ICON);
+        TabTable.helpIcon = icon.registerIcon(ConstantValue.HELP_ICON);
+        TabJournal.helpIcon = icon.registerIcon(ConstantValue.POWER_ICON);
+    }
+
+    @ForgeSubscribe
+    public void onPreRender(RenderGameOverlayEvent.Pre e)
+    {
+        TickHandler.renderEffects();
+    }
 }
