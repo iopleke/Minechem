10af6b3b83f7cc50ddc50e6b09debd085dda18e4
Maxwolf Goodliffe <devs@madsciencemod.com>
2014-05-15 03:46:10 -0700

    Added latest Universal Electricity API.

---
+++ src/main/java/universalelectricity/api/vector/IVector2.java
@@ -0,0 +1,14 @@
+package universalelectricity.api.vector;
+
+import net.minecraft.world.World;
+
+/** Useful interface to define that an object has a 2D location.
+ * 
+ * @author DarkGuardsman */
+public interface IVector2
+{
+    
+    double x();
+
+    double y();
+}
---
+++ src/main/java/universalelectricity/api/vector/IVector3.java
@@ -0,0 +1,10 @@
+package universalelectricity.api.vector;
+
+
+/** Useful interface to define that an object has a 3D location.
+ * 
+ * @author DarkGuardsman */
+public interface IVector3 extends IVector2
+{
+    double z();
+}
