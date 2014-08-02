b1559258d0181eda623cb38d45aa9097bcc90115
jakimfett <jakimfett@picraft.com>
2014-08-01 22:30:59 -0700

    Fix breakBlock, fix imports

--- src/main/java/minechem/block/BasicComplexBlock.java
+++ src/main/java/minechem/block/BasicComplexBlock.java
@@ -2,20 +2,23 @@ package minechem.block;
 
 import java.util.ArrayList;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import java.util.Random;
+import minechem.ModMinechem;
 import minechem.network.server.CommonProxy;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import minechem.ModMinechem;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.util.IIcon;
 
 public abstract class BasicComplexBlock extends Block implements IBlock
 {
@@ -61,6 +64,45 @@ public abstract class BasicComplexBlock extends Block implements IBlock
     }
 
     public abstract void addStacksDroppedOnBlockBreak(TileEntity tileEntity, ArrayList<ItemStack> itemStacks);
+
+    @Override
+    public void breakBlock(World world, int x, int y, int z, Block block, int metaData)
+    {
+        Random random = new Random();
+        TileEntity tileEntity = world.getTileEntity(x, y, z);
+        if (tileEntity != null)
+        {
+            ArrayList<ItemStack> droppedStacks = new ArrayList<ItemStack>();
+            addStacksDroppedOnBlockBreak(tileEntity, droppedStacks);
+            for (ItemStack itemstack : droppedStacks)
+            {
+                float randomX = random.nextFloat() * 0.8F + 0.1F;
+                float randomY = random.nextFloat() * 0.8F + 0.1F;
+                float randomZ = random.nextFloat() * 0.8F + 0.1F;
+                while (itemstack.stackSize > 0)
+                {
+                    int randomN = random.nextInt(21) + 10;
+                    if (randomN > itemstack.stackSize)
+                    {
+                        randomN = itemstack.stackSize;
+                    }
+                    itemstack.stackSize -= randomN;
+                    ItemStack droppedStack = new ItemStack(itemstack.getItem(), randomN, itemstack.getItemDamage());
+                    // Copy NBT tag data, needed to preserve Chemist Journal
+                    // contents (for example).
+                    if (itemstack.hasTagCompound())
+                    {
+                        droppedStack.setTagCompound((NBTTagCompound) itemstack.getTagCompound().copy());
+                    }
+
+                    EntityItem droppedEntityItem = new EntityItem(world, x + randomX, y + randomY, z + randomZ, droppedStack);
+                    world.spawnEntityInWorld(droppedEntityItem);
+                }
+            }
+            super.breakBlock(world, x, y, z, block, metaData);
+        }
+
+    }
 
     @SideOnly(Side.CLIENT)
     @Override
