67e7e719b2acc21e6e633251ef280413026afb35
jakimfett <jakimfett@picraft.com>
2014-08-01 22:15:33 -0700

    Update to 1.7 syntax and overrides

--- src/main/java/minechem/block/BasicComplexBlock.java
+++ src/main/java/minechem/block/BasicComplexBlock.java
@@ -1,29 +1,27 @@
 package minechem.block;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 import minechem.network.server.CommonProxy;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Icon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import minechem.ModMinechem;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
 
 public abstract class BasicComplexBlock extends Block implements IBlock
 {
 
-    public Icon connectorIcon;
-    public Icon topIcon;
+    public IIcon connectorIcon;
+    public IIcon topIcon;
     static int blockIdIncrement;
     public String textureBase = "minechem:";
 
@@ -48,19 +46,11 @@ public abstract class BasicComplexBlock extends Block implements IBlock
         return false;
     }
 
-    public BasicComplexBlock(int id, Material material)
+    public BasicComplexBlock(Material material)
     {
-        super(id, material);
+        super(material);
 
-        this.setUnlocalizedName(this.getName());
-
-    }
-
-    public BasicComplexBlock(int id)
-    {
-        super(id, Material.iron);
-
-        this.setUnlocalizedName(this.getName());
+        this.setCreativeTab(ModMinechem.CREATIVE_TAB);
 
     }
 
@@ -71,43 +61,6 @@ public abstract class BasicComplexBlock extends Block implements IBlock
     }
 
     public abstract void addStacksDroppedOnBlockBreak(TileEntity tileEntity, ArrayList<ItemStack> itemStacks);
-
-    @Override
-    public void breakBlock(World world, int x, int y, int z, int oldBlockId, int oldMetadata)
-    {
-        Random random = new Random();
-        TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-        if (tileEntity != null)
-        {
-            ArrayList<ItemStack> droppedStacks = new ArrayList<ItemStack>();
-            addStacksDroppedOnBlockBreak(tileEntity, droppedStacks);
-            for (ItemStack itemstack : droppedStacks)
-            {
-                float randomX = random.nextFloat() * 0.8F + 0.1F;
-                float randomY = random.nextFloat() * 0.8F + 0.1F;
-                float randomZ = random.nextFloat() * 0.8F + 0.1F;
-                while (itemstack.stackSize > 0)
-                {
-                    int randomN = random.nextInt(21) + 10;
-                    if (randomN > itemstack.stackSize)
-                        randomN = itemstack.stackSize;
-                    itemstack.stackSize -= randomN;
-                    ItemStack droppedStack = new ItemStack(itemstack.itemID, randomN, itemstack.getItemDamage());
-                    // Copy NBT tag data, needed to preserve Chemist Journal
-                    // contents (for example).
-                    if (itemstack.hasTagCompound())
-                    {
-                        droppedStack.setTagCompound((NBTTagCompound) itemstack.getTagCompound().copy());
-                    }
-
-                    EntityItem droppedEntityItem = new EntityItem(world, x + randomX, y + randomY, z + randomZ, droppedStack);
-                    world.spawnEntityInWorld(droppedEntityItem);
-                }
-            }
-            super.breakBlock(world, x, y, z, oldBlockId, oldMetadata);
-        }
-
-    }
 
     @SideOnly(Side.CLIENT)
     @Override
@@ -122,7 +75,7 @@ public abstract class BasicComplexBlock extends Block implements IBlock
 
     @Override
     @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
+    public void registerBlockIcons(IIconRegister par1IconRegister)
     {
 
         blockIcon = par1IconRegister.registerIcon(textureBase + this.getFront());
@@ -130,7 +83,7 @@ public abstract class BasicComplexBlock extends Block implements IBlock
     }
 
     @Override
-    public Icon getIcon(int side, int meta)
+    public IIcon getIcon(int side, int meta)
     {
         if (topSidedTextures())
         {
@@ -145,7 +98,7 @@ public abstract class BasicComplexBlock extends Block implements IBlock
     }
 
     @Override
-    public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLivingBase par5EntityLiving, ItemStack par6ItemStack)
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase par5EntityLiving, ItemStack par6ItemStack)
     {
 
         int angle = MathHelper.floor_double((par5EntityLiving.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
@@ -153,21 +106,21 @@ public abstract class BasicComplexBlock extends Block implements IBlock
 
         switch (angle)
         {
-        case 0:
-            change = 1;
-            break;
-        case 1:
-            change = 2;
-            break;
-        case 2:
-            change = 0;
-            break;
-        case 3:
-            change = 3;
-            break;
+            case 0:
+                change = 1;
+                break;
+            case 1:
+                change = 2;
+                break;
+            case 2:
+                change = 0;
+                break;
+            case 3:
+                change = 3;
+                break;
         }
-        par1World.setBlockMetadataWithNotify(x, y, z, change, 2);
-        par1World.notifyBlocksOfNeighborChange(x, y, z, this.blockID);
+        world.setBlockMetadataWithNotify(x, y, z, change, 2);
+        world.notifyBlocksOfNeighborChange(x, y, z, this);
     }
 
     @Override
@@ -176,8 +129,7 @@ public abstract class BasicComplexBlock extends Block implements IBlock
         try
         {
             return getTileEntityClass().newInstance();
-        }
-        catch (Throwable e)
+        } catch (Throwable e)
         {
             // TODO Auto-generated catch block
             e.printStackTrace();
