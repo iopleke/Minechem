7ed22640f5886ee8ad92b55a7af2aced6475c384
jakimfett <jakimfett@picraft.com>
2014-08-01 23:34:51 -0700

    Change to use BlockSimple

--- src/main/java/minechem/block/uranium/BlockUraniumOre.java
+++ src/main/java/minechem/block/uranium/BlockUraniumOre.java
@@ -1,35 +1,17 @@
 package minechem.block.uranium;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import minechem.ModMinechem;
+import minechem.block.BlockSimple;
 import minechem.utils.Reference;
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.util.IIcon;
 
-public class BlockUraniumOre extends Block
+public class BlockUraniumOre extends BlockSimple
 {
     public BlockUraniumOre()
     {
-        super(Material.iron);
+        super(Material.iron, Reference.URANIUM_TEX, 1, "Uranium Ore");
         this.setCreativeTab(ModMinechem.CREATIVE_TAB);
         //this.setBlockName("Uranium Ore");
         this.setHardness(4F);
-    }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void registerBlockIcons(IIconRegister p_149651_1_)
-    {
-        this.blockIcon = p_149651_1_.registerIcon(Reference.URANIUM_TEX);
-    }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public IIcon getIcon(int p_149691_1_, int p_149691_2_)
-    {
-        return this.blockIcon;
     }
 }
