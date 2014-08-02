5767cd3b94a8113c4a83e02f7df892f65e69b74c
jakimfett <jakimfett@picraft.com>
2014-08-02 11:41:06 -0700

    Fix ScaledResolution call

--- src/main/java/minechem/gui/ScissorHelper.java
+++ src/main/java/minechem/gui/ScissorHelper.java
@@ -8,15 +8,15 @@ import org.lwjgl.opengl.GL11;
 public class ScissorHelper
 {
 
-    public static void startScissor(Minecraft mc, int x, int y, int w, int h)
+    public static void startScissor(Minecraft minecraft, int x, int y, int w, int h)
     {
-        ScaledResolution scaledRes = new ScaledResolution(mc.gameSettings, mc.displayWidth, mc.displayHeight);
+        ScaledResolution scaledRes = new ScaledResolution(minecraft, minecraft.displayWidth, minecraft.displayHeight);
         int scale = scaledRes.getScaleFactor();
 
         int scissorWidth = w * scale;
         int scissorHeight = h * scale;
         int scissorX = x * scale;
-        int scissorY = mc.displayHeight - scissorHeight - (y * scale);
+        int scissorY = minecraft.displayHeight - scissorHeight - (y * scale);
 
         GL11.glEnable(GL11.GL_SCISSOR_TEST);
         GL11.glScissor(scissorX, scissorY, scissorWidth, scissorHeight);
