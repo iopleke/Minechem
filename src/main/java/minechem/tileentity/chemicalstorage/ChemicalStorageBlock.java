9570e9d020204e299b44571090cd4614664505fc
jakimfett <kstraw@onkoistudios.com>
2014-08-20 11:34:43 -0700

    Add import for ChatComponentText

--- src/main/java/minechem/tileentity/chemicalstorage/ChemicalStorageBlock.java
+++ src/main/java/minechem/tileentity/chemicalstorage/ChemicalStorageBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
