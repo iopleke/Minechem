511b9dd644d52234db94a233054a8e83ddedc01e
jakimfett <jakimfett@picraft.com>
2014-08-22 20:58:00 -0700

    Fix indentation

--- src/main/java/minechem/MinechemGeneration.java
+++ src/main/java/minechem/MinechemGeneration.java
@@ -13,21 +13,20 @@ public class MinechemGeneration implements IWorldGenerator
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
     {
-            if (Settings.WorldGenOre)
+        if (Settings.WorldGenOre)
+        {
+            if (world.provider.isSurfaceWorld())
             {
-                if (world.provider.isSurfaceWorld())
+                for (int k = 0; k < 2; k++)
                 {
-                    for (int k = 0; k < 2; k++)
-                    {
-                        int firstBlockXCoord = 16 * chunkX + random.nextInt(16);
-                        int firstBlockYCoord = random.nextInt(30);
-                        int firstBlockZCoord = 16 * chunkZ + random.nextInt(16);
-                        WorldGenMinable mineable = new WorldGenMinable(MinechemBlocksGeneration.uranium.blockID, 4);
-                        mineable.generate(world, random, firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
-                    }
+                    int firstBlockXCoord = 16 * chunkX + random.nextInt(16);
+                    int firstBlockYCoord = random.nextInt(30);
+                    int firstBlockZCoord = 16 * chunkZ + random.nextInt(16);
+                    WorldGenMinable mineable = new WorldGenMinable(MinechemBlocksGeneration.uranium.blockID, 4);
+                    mineable.generate(world, random, firstBlockXCoord, firstBlockYCoord, firstBlockZCoord);
                 }
             }
-    
-        
+        }
+
     }
 }
