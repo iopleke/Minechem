05c7d6efc24737d72c166adbbf5dfdf630508cd3
jakimfett <jakimfett@picraft.com>
2014-09-02 19:26:22 -0700

    Fix code whitespace and formatting

--- src/main/java/minechem/tileentity/multiblock/ghostblock/GhostBlock.java
+++ src/main/java/minechem/tileentity/multiblock/ghostblock/GhostBlock.java
@@ -22,7 +22,7 @@ public class GhostBlock extends BlockContainer
 
     public GhostBlock()
     {
-        super(Material.iron);
+        super(Material.snow);
         setBlockName("ghostBlock");
         setLightLevel(0.5F);
         setHardness(1000F);
@@ -37,10 +37,10 @@ public class GhostBlock extends BlockContainer
     {
         switch (metadata)
         {
-        case 0:
-            return icon1;
-        case 1:
-            return icon2;
+            case 0:
+                return icon1;
+            case 1:
+                return icon2;
         }
         return blockIcon;
     }
@@ -60,10 +60,14 @@ public class GhostBlock extends BlockContainer
         super.onBlockActivated(world, x, y, z, entityPlayer, side, par7, par8, par9);
 
         if (world.isRemote)
+        {
             return true;
+        }
 
         if (entityPlayer.getDistanceSq(x + 0.5D, y + 0.5D, z + 0.5D) > 64.0D)
+        {
             return true;
+        }
 
         TileEntity tileEntity = world.getTileEntity(x, y, z);
         if (tileEntity instanceof GhostBlockTileEntity)
@@ -73,22 +77,27 @@ public class GhostBlock extends BlockContainer
             if (playerIsHoldingItem(entityPlayer, blockAsStack))
             {
 
-                world.setBlock(x, y, z, /*blockAsStack.getItem()*/this, 0, 0);
+                world.setBlock(x, y, z, /*blockAsStack.getItem()*/ this, 0, 0);
                 world.setBlock(x, y, z, this, blockAsStack.getItemDamage(), 3);
                 if (!entityPlayer.capabilities.isCreativeMode)
+                {
                     entityPlayer.inventory.decrStackSize(entityPlayer.inventory.currentItem, 1);
+                }
                 return true;
             }
         }
         return false;
     }
 
-    private boolean playerIsHoldingItem(EntityPlayer entityPlayer, ItemStack itemstack) 
+    private boolean playerIsHoldingItem(EntityPlayer entityPlayer, ItemStack itemstack)
     {
         ItemStack helditem = entityPlayer.inventory.getCurrentItem();
-        if (helditem != null && itemstack != null) {
-            if (helditem.getItem() == itemstack.getItem()) {
-                if (helditem.getItemDamage() == itemstack.getItemDamage() || itemstack.getItemDamage() == -1) {
+        if (helditem != null && itemstack != null)
+        {
+            if (helditem.getItem() == itemstack.getItem())
+            {
+                if (helditem.getItemDamage() == itemstack.getItemDamage() || itemstack.getItemDamage() == -1)
+                {
                     return true;
                 }
             }
@@ -96,7 +105,9 @@ public class GhostBlock extends BlockContainer
         return false;
     }
 
-    /** Returns whether this block is collideable based on the arguments passed in Args: blockMetaData, unknownFlag */
+    /**
+     * Returns whether this block is collideable based on the arguments passed in Args: blockMetaData, unknownFlag
+     */
     @Override
     public boolean canCollideCheck(int par1, boolean par2)
     {
@@ -109,21 +120,27 @@ public class GhostBlock extends BlockContainer
         return par1;
     }
 
-    /** Triggered whenever an entity collides with this block (enters into the block). Args: world, x, y, z, entity */
+    /**
+     * Triggered whenever an entity collides with this block (enters into the block). Args: world, x, y, z, entity
+     */
     @Override
     public void onEntityCollidedWithBlock(World par1World, int par2, int par3, int par4, Entity par5Entity)
     {
         // Todo
     }
 
-    /** Returns a bounding box from the pool of bounding boxes (this means this box can change after the pool has been cleared to be reused) */
+    /**
+     * Returns a bounding box from the pool of bounding boxes (this means this box can change after the pool has been cleared to be reused)
+     */
     @Override
     public AxisAlignedBB getCollisionBoundingBoxFromPool(World par1World, int par2, int par3, int par4)
     {
         return null;
     }
 
-    /** Returns true if the given side of this block type should be rendered, if the adjacent block is at the given coordinates. Args: blockAccess, x, y, z, side */
+    /**
+     * Returns true if the given side of this block type should be rendered, if the adjacent block is at the given coordinates. Args: blockAccess, x, y, z, side
+     */
     @Override
     public boolean shouldSideBeRendered(IBlockAccess par1IBlockAccess, int par2, int par3, int par4, int par5)
     {
@@ -142,7 +159,9 @@ public class GhostBlock extends BlockContainer
         return false;
     }
 
-    /** Returns which pass should this block be rendered on. 0 for solids and 1 for alpha */
+    /**
+     * Returns which pass should this block be rendered on. 0 for solids and 1 for alpha
+     */
     @Override
     @SideOnly(Side.CLIENT)
     public int getRenderBlockPass()
