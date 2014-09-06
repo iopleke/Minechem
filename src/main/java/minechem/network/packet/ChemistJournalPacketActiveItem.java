baead85b4d06985185a856dff00c667509c6e7a6
jakimfett <jakimfett@picraft.com>
2014-09-06 12:21:06 -0700

    Fix code whitespace

--- src/main/java/minechem/network/packet/ChemistJournalPacketActiveItem.java
+++ src/main/java/minechem/network/packet/ChemistJournalPacketActiveItem.java
@@ -16,13 +16,19 @@ import java.io.IOException;
 
 public class ChemistJournalPacketActiveItem extends PacketDispatcher.AbstractPacket<ChemistJournalPacketActiveItem>
 {
-    /** Item ID for the currently active item in the players chemist journal. */
+    /**
+     * Item ID for the currently active item in the players chemist journal.
+     */
     private Item item;
-    
-    /** Damage value for the active item for metadata tracking purposes. */
+
+    /**
+     * Damage value for the active item for metadata tracking purposes.
+     */
     private int itemDMG;
-    
-    /** Reference to the slot number in the chemist journal that this item corresponds to. */
+
+    /**
+     * Reference to the slot number in the chemist journal that this item corresponds to.
+     */
     private int slot;
 
     public ChemistJournalPacketActiveItem(ItemStack activeStack, EntityPlayer player)
@@ -38,34 +44,37 @@ public class ChemistJournalPacketActiveItem extends PacketDispatcher.AbstractPac
         // Required for reflection.
     }
 
-	@Override
-	public void sendPacket(PacketBuffer out) throws IOException {
-		// Chemist Journal specific from the client instance of the item.
-		out.writeItemStackToBuffer(new ItemStack(this.item));//TODO: Double check to see if getIdFromItem is still in used or possible use string
-		out.writeInt(this.itemDMG);
-		out.writeInt(this.slot);
-	}
+    @Override
+    public void sendPacket(PacketBuffer out) throws IOException
+    {
+        // Chemist Journal specific from the client instance of the item.
+        out.writeItemStackToBuffer(new ItemStack(this.item));//TODO: Double check to see if getIdFromItem is still in used or possible use string
+        out.writeInt(this.itemDMG);
+        out.writeInt(this.slot);
+    }
 
-	@Override
-	public void receivePacket(PacketBuffer in) throws IOException {
-		this.item = in.readItemStackFromBuffer().getItem();
-		this.itemDMG = in.readInt();
-		this.slot = in.readInt();
-	}
+    @Override
+    public void receivePacket(PacketBuffer in) throws IOException
+    {
+        this.item = in.readItemStackFromBuffer().getItem();
+        this.itemDMG = in.readInt();
+        this.slot = in.readInt();
+    }
 
-	@Override//TODO: Side Server
-	public void processPacket(EntityPlayer player) {
-		// Grab the chemist journal out of the players server world instance inventory.
-		ItemStack journal = player.inventory.mainInventory[this.slot];
+    @Override//TODO: Side Server
+    public void processPacket(EntityPlayer player)
+    {
+        // Grab the chemist journal out of the players server world instance inventory.
+        ItemStack journal = player.inventory.mainInventory[this.slot];
 
-		// Check that it is indeed a journal and matches the ID we have internally for that item.
-		if (journal != null && journal.getItem() == MinechemItemsRegistration.journal)
-		{
-			// Set the active damage amount for the active item to ensure meta-data items show up properly.
-			ItemStack activeStack = new ItemStack(this.item, 1, this.itemDMG);
+        // Check that it is indeed a journal and matches the ID we have internally for that item.
+        if (journal != null && journal.getItem() == MinechemItemsRegistration.journal)
+        {
+            // Set the active damage amount for the active item to ensure meta-data items show up properly.
+            ItemStack activeStack = new ItemStack(this.item, 1, this.itemDMG);
 
-			// Sets the active stack in the server instance of the chemists journal.
-			MinechemItemsRegistration.journal.setActiveStack(activeStack, journal);
-		}
-	}
+            // Sets the active stack in the server instance of the chemists journal.
+            MinechemItemsRegistration.journal.setActiveStack(activeStack, journal);
+        }
+    }
 }
