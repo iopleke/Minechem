29ba7c3195a744746d61f3f7d73ec9a9eabfeba6
jakimfett <jakimfett@picraft.com>
2014-09-11 12:43:45 -0700

    Add version separator

--- src/main/java/minechem/Minechem.java
+++ src/main/java/minechem/Minechem.java
@@ -118,7 +118,7 @@ public class Minechem
         metadata.description = Minechem.NAME + " is a mod about chemistry, allowing you to research blocks and items, and then break them down into their base compounds and elements.";
         metadata.url = "http://www.minechemmod.com/";
         metadata.logoFile = "assets/" + Minechem.ID + "/logo.png";
-        metadata.version = V_MAJOR + "." + V_MINOR + V_REVIS;
+        metadata.version = V_MAJOR + "." + V_MINOR + "." + V_REVIS;
         metadata.authorList = Arrays.asList(new String[]
         {
             "pixlepix", "jakimfett", "maxwolf"
