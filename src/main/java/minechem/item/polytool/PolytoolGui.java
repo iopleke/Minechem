dec7eb1451efeb3e0d6a5c4df8faacd8a35c53a4
jakimfett <jakimfett@picraft.com>
2014-09-22 22:55:44 -0700

    Refactor par1 variable name

--- src/main/java/minechem/item/polytool/PolytoolGui.java
+++ src/main/java/minechem/item/polytool/PolytoolGui.java
@@ -23,75 +23,79 @@ import org.lwjgl.opengl.GL11;
 
 public class PolytoolGui extends GuiContainerTabbed
 {
-    private static final ResourceLocation texture = new ResourceLocation("minechem", "textures/gui/polytool.png");
-    public ArrayList<ElementGuiHelper> elements = new ArrayList();
-    long renders;
-    ItemStack polytool;
-    InventoryPlayer player;
+	private static final ResourceLocation texture = new ResourceLocation("minechem", "textures/gui/polytool.png");
+	public ArrayList<ElementGuiHelper> elements = new ArrayList();
+	long renders;
+	ItemStack polytool;
+	InventoryPlayer player;
 	boolean shouldUpdate;
 
-    public PolytoolGui(PolytoolContainer par1Container)
-    {
-        super(par1Container);
-        xSize = 176;
-        ySize = 218;
-        this.polytool = par1Container.player.getCurrentItem();
-        this.player = par1Container.player;
+	public PolytoolGui(PolytoolContainer polytoolContainer)
+	{
+		super(polytoolContainer);
+		xSize = 176;
+		ySize = 218;
+		this.polytool = polytoolContainer.player.getCurrentItem();
+		this.player = polytoolContainer.player;
 		this.shouldUpdate = true;
-        ItemStack stack = par1Container.player.getCurrentItem();
-        if (stack.getItem() instanceof PolytoolItem)
-        {
-            ArrayList<PolytoolUpgradeType> upgrades = PolytoolItem.getUpgrades(stack);
-            Iterator<PolytoolUpgradeType> iter = upgrades.iterator();
-            Random rand = new Random();
-            while (iter.hasNext())
-            {
-                PolytoolUpgradeType upgrade = iter.next();
-                ElementEnum element = upgrade.getElement();
-                for (int i = 0; i < upgrade.power; i++)
-                {
+		ItemStack stack = polytoolContainer.player.getCurrentItem();
+		if (stack.getItem() instanceof PolytoolItem)
+		{
+			ArrayList<PolytoolUpgradeType> upgrades = PolytoolItem.getUpgrades(stack);
+			Iterator<PolytoolUpgradeType> iter = upgrades.iterator();
+			Random rand = new Random();
+			while (iter.hasNext())
+			{
+				PolytoolUpgradeType upgrade = iter.next();
+				ElementEnum element = upgrade.getElement();
+				for (int i = 0; i < upgrade.power; i++)
+				{
 
-                    elements.add(new ElementGuiHelper(1 + rand.nextInt(2), rand.nextDouble() * Math.PI * 2, element));
-                }
+					elements.add(new ElementGuiHelper(1 + rand.nextInt(2), rand.nextDouble() * Math.PI * 2, element));
+				}
 
-            }
-        }
-        addTab(new GuiTabHelp(this, MinechemHelper.getLocalString("help.polytool")));
+			}
+		}
+		addTab(new GuiTabHelp(this, MinechemHelper.getLocalString("help.polytool")));
 
-    }
+	}
 
-    public void drawItemStack(ItemStack par1ItemStack, int par2, int par3, String par4Str)
-    {
-        par2 += guiLeft;
-        par3 += guiTop;
+	public void drawItemStack(ItemStack par1ItemStack, int par2, int par3, String par4Str)
+	{
+		par2 += guiLeft;
+		par3 += guiTop;
 
-        // Copied from GuiContainer
-        GL11.glTranslatef(0.0F, 0.0F, 32.0F);
-        this.zLevel = 3;
-        itemRender.zLevel = 200.0F;
-        FontRenderer font = null;
-        if (par1ItemStack != null)
-            font = par1ItemStack.getItem().getFontRenderer(par1ItemStack);
-        if (font == null)
-            font = fontRendererObj;
-        itemRender.renderItemAndEffectIntoGUI(font, this.mc.getTextureManager(), par1ItemStack, par2, par3);
-        itemRender.renderItemOverlayIntoGUI(font, this.mc.getTextureManager(), par1ItemStack, par2, par3 - (8), par4Str);
-        this.zLevel = 3;
-        itemRender.zLevel = 0.0F;
-    }
+		// Copied from GuiContainer
+		GL11.glTranslatef(0.0F, 0.0F, 32.0F);
+		this.zLevel = 3;
+		itemRender.zLevel = 200.0F;
+		FontRenderer font = null;
+		if (par1ItemStack != null)
+		{
+			font = par1ItemStack.getItem().getFontRenderer(par1ItemStack);
+		}
+		if (font == null)
+		{
+			font = fontRendererObj;
+		}
+		itemRender.renderItemAndEffectIntoGUI(font, this.mc.getTextureManager(), par1ItemStack, par2, par3);
+		itemRender.renderItemOverlayIntoGUI(font, this.mc.getTextureManager(), par1ItemStack, par2, par3 - (8), par4Str);
+		this.zLevel = 3;
+		itemRender.zLevel = 0.0F;
+	}
 
-    @Override
-    protected void drawGuiContainerBackgroundLayer(float f, int mouseX, int mouseY)
-    {
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float f, int mouseX, int mouseY)
+	{
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 
-        Minecraft.getMinecraft().getTextureManager().bindTexture(texture);
+		Minecraft.getMinecraft().getTextureManager().bindTexture(texture);
 
-        drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
-        renders++;
-		
+		drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+		renders++;
+
 		ItemStack polytoolTemp = FMLClientHandler.instance().getClientPlayerEntity().getCurrentEquippedItem();
-		
+
 		ArrayList<ElementGuiHelper> elementsUpdate = new ArrayList();
 		ArrayList<PolytoolUpgradeType> upgrades = PolytoolItem.getUpgrades(polytoolTemp);
 		Iterator<PolytoolUpgradeType> iter = upgrades.iterator();
@@ -107,80 +111,81 @@ public class PolytoolGui extends GuiContainerTabbed
 			}
 
 		}
-		
+
 		boolean sizeEqual = elements.size() == elementsUpdate.size();
-		
-		if(!sizeEqual || this.shouldUpdate){
+
+		if (!sizeEqual || this.shouldUpdate)
+		{
 			this.elements = elementsUpdate;
 			this.shouldUpdate = false;
 		}
-		
+
 		Iterator renderIter = elements.iterator();
-        while (renderIter.hasNext())
-        {
-            ((ElementGuiHelper) renderIter.next()).draw(this, renders);
-        }
+		while (renderIter.hasNext())
+		{
+			((ElementGuiHelper) renderIter.next()).draw(this, renders);
+		}
 
-        drawItemStack(new ItemStack(MinechemItemsRegistration.polytool), 80, 42, "");
-        String localizedName;
+		drawItemStack(new ItemStack(MinechemItemsRegistration.polytool), 80, 42, "");
+		String localizedName;
 
-        localizedName=MinechemHelper.getLocalString("gui.polytool.sword");
-        if(localizedName.isEmpty() || localizedName=="gui.polytool.sword")
-        {
-        	localizedName="Sword";
-        }
-        fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getSwordStr(polytool), guiLeft + 10, guiTop + 80, 0x404040);
+		localizedName = MinechemHelper.getLocalString("gui.polytool.sword");
+		if (localizedName.isEmpty() || localizedName == "gui.polytool.sword")
+		{
+			localizedName = "Sword";
+		}
+		fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getSwordStr(polytool), guiLeft + 10, guiTop + 80, 0x404040);
 
-        localizedName=MinechemHelper.getLocalString("gui.polytool.ores");
-        if(localizedName.isEmpty() || localizedName=="gui.polytool.ores")
-        {
-        	localizedName="Ores";
-        }
-        fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getPickaxeStr(polytool), guiLeft + 10, guiTop + 90, 0x404040);
+		localizedName = MinechemHelper.getLocalString("gui.polytool.ores");
+		if (localizedName.isEmpty() || localizedName == "gui.polytool.ores")
+		{
+			localizedName = "Ores";
+		}
+		fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getPickaxeStr(polytool), guiLeft + 10, guiTop + 90, 0x404040);
 
-        localizedName=MinechemHelper.getLocalString("gui.polytool.stone");
-        if(localizedName.isEmpty() || localizedName=="gui.polytool.stone")
-        {
-        	localizedName="Stone";
-        }
-        fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getStoneStr(polytool), guiLeft + 10, guiTop + 100, 0x404040);
+		localizedName = MinechemHelper.getLocalString("gui.polytool.stone");
+		if (localizedName.isEmpty() || localizedName == "gui.polytool.stone")
+		{
+			localizedName = "Stone";
+		}
+		fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getStoneStr(polytool), guiLeft + 10, guiTop + 100, 0x404040);
 
-        localizedName=MinechemHelper.getLocalString("gui.polytool.axe");
-        if(localizedName.isEmpty() || localizedName=="gui.polytool.axe")
-        {
-        	localizedName="Axe";
-        }
-        fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getAxeStr(polytool), guiLeft + 10, guiTop + 110, 0x404040);
+		localizedName = MinechemHelper.getLocalString("gui.polytool.axe");
+		if (localizedName.isEmpty() || localizedName == "gui.polytool.axe")
+		{
+			localizedName = "Axe";
+		}
+		fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getAxeStr(polytool), guiLeft + 10, guiTop + 110, 0x404040);
 
-        localizedName=MinechemHelper.getLocalString("gui.polytool.shovel");
-        if(localizedName.isEmpty() || localizedName=="gui.polytool.shovel")
-        {
-        	localizedName="Shovel";
-        }
-        fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getShovelStr(polytool), guiLeft + 10, guiTop + 120, 0x404040);
-    }
+		localizedName = MinechemHelper.getLocalString("gui.polytool.shovel");
+		if (localizedName.isEmpty() || localizedName == "gui.polytool.shovel")
+		{
+			localizedName = "Shovel";
+		}
+		fontRendererObj.drawString(localizedName + ": " + PolytoolItem.instance.getShovelStr(polytool), guiLeft + 10, guiTop + 120, 0x404040);
+	}
 
-    public void addUpgrade(PolytoolUpgradeType upgrade)
-    {
-        Random rand = new Random();
-        for (int i = 0; i < upgrade.power; i++)
-        {
-            elements.add(new ElementGuiHelper(1 + rand.nextInt(2), rand.nextDouble() * Math.PI * 2, upgrade.getElement()));
+	public void addUpgrade(PolytoolUpgradeType upgrade)
+	{
+		Random rand = new Random();
+		for (int i = 0; i < upgrade.power; i++)
+		{
+			elements.add(new ElementGuiHelper(1 + rand.nextInt(2), rand.nextDouble() * Math.PI * 2, upgrade.getElement()));
 			this.shouldUpdate = true;
-        }
-    }
+		}
+	}
 
 	@Override
-	public boolean hideItemPanelSlot(GuiContainer gui, int x, int y, int w, int h) 
+	public boolean hideItemPanelSlot(GuiContainer gui, int x, int y, int w, int h)
 	{
 		return false;
 	}
 
-    @Optional.Method(modid = "NotEnoughItems")
-    @Override
-    public Iterable<Integer> getItemSpawnSlots(GuiContainer gc, ItemStack is)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Optional.Method(modid = "NotEnoughItems")
+	@Override
+	public Iterable<Integer> getItemSpawnSlots(GuiContainer gc, ItemStack is)
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
 
 }
