2f3699bb5daf834701bf7d081e891ca5a8ecefd5
jakimfett <jakimfett@picraft.com>
2014-09-23 17:55:50 -0700

    Move logging to debug only

--- src/main/java/minechem/Minechem.java
+++ src/main/java/minechem/Minechem.java
@@ -45,176 +45,230 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Minechem.ID, name = Minechem.NAME, version = Minechem.VERSION_FULL, useMetadata = false, guiFactory = "minechem.gui.GuiFactory", acceptedMinecraftVersions = "[1.7.10,)", dependencies = "required-after:Forge@[10.13.0.1180,);after:BuildCraft|Energy;after:factorization;after:IC2;after:Railcraft;after:ThermalExpansion")
 public class Minechem
 {
-    // Internal mod name used for reference purposes and resource gathering.
-    public static final String ID = "minechem";
+	// Internal mod name used for reference purposes and resource gathering.
+	public static final String ID = "minechem";
 
-    // Network name that is used in the NetworkMod.
-    public static final String CHANNEL_NAME = ID;
+	// Network name that is used in the NetworkMod.
+	public static final String CHANNEL_NAME = ID;
 
-    // User friendly version of our mods name.
-    public static final String NAME = "Minechem";
+	// User friendly version of our mods name.
+	public static final String NAME = "Minechem";
 
-    // Main version information that will be displayed in mod listing and for other purposes.
-    public static final String V_MAJOR = "@MAJOR@";
-    public static final String V_MINOR = "@MINOR@";
-    public static final String V_REVIS = "@REVIS@";
-    public static final String V_BUILD = "@BUILD@";
-    public static final String VERSION_FULL = V_MAJOR + "." + V_MINOR + "." + V_REVIS + "." + V_BUILD;
+	// Main version information that will be displayed in mod listing and for other purposes.
+	public static final String V_MAJOR = "@MAJOR@";
+	public static final String V_MINOR = "@MINOR@";
+	public static final String V_REVIS = "@REVIS@";
+	public static final String V_BUILD = "@BUILD@";
+	public static final String VERSION_FULL = V_MAJOR + "." + V_MINOR + "." + V_REVIS + "." + V_BUILD;
 
-    // Misc variables
-    public static final String textureBase = "minechem:";
+	// Misc variables
+	public static final String textureBase = "minechem:";
 
-    // Instancing
-    @Instance(value = CHANNEL_NAME)
-    public static Minechem INSTANCE;
+	// Instancing
+	@Instance(value = CHANNEL_NAME)
+	public static Minechem INSTANCE;
 
-    // Provides standard logging from the Forge.
-    // public static Logger LOGGER;
-    // Public extra data about our mod that Forge uses in the mods listing page for more information.
-    @Mod.Metadata(Minechem.ID)
-    public static ModMetadata metadata;
+	// Provides standard logging from the Forge.
+	// public static Logger LOGGER;
+	// Public extra data about our mod that Forge uses in the mods listing page for more information.
+	@Mod.Metadata(Minechem.ID)
+	public static ModMetadata metadata;
 
-    @SidedProxy(clientSide = "minechem.proxy.ClientProxy", serverSide = "minechem.proxy.CommonProxy")
-    public static CommonProxy PROXY;
+	@SidedProxy(clientSide = "minechem.proxy.ClientProxy", serverSide = "minechem.proxy.CommonProxy")
+	public static CommonProxy PROXY;
 
-    // Creative mode tab that shows up in Minecraft.
-    public static CreativeTabs CREATIVE_TAB_ITEMS = new CreativeTabMinechem(Minechem.NAME);
-    public static CreativeTabs CREATIVE_TAB_ELEMENTS = new CreativeTabMinechem(Minechem.NAME + ".Elements");
+	// Creative mode tab that shows up in Minecraft.
+	public static CreativeTabs CREATIVE_TAB_ITEMS = new CreativeTabMinechem(Minechem.NAME);
+	public static CreativeTabs CREATIVE_TAB_ELEMENTS = new CreativeTabMinechem(Minechem.NAME + ".Elements");
 
-    public static final ResourceLocation ICON_ENERGY = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_power.png");
-    public static final ResourceLocation ICON_FULL_ENERGY = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_fullEower.png");
-    public static final ResourceLocation ICON_HELP = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_help.png");
-    public static final ResourceLocation ICON_JAMMED = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_jammed.png");
-    public static final ResourceLocation ICON_NO_BOTTLES = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_noBottles.png");
-    public static final ResourceLocation ICON_NO_RECIPE = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_noRecipe.png");
-    public static final ResourceLocation ICON_NO_ENERGY = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_unpowered.png");
+	public static final ResourceLocation ICON_ENERGY = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_power.png");
+	public static final ResourceLocation ICON_FULL_ENERGY = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_fullEower.png");
+	public static final ResourceLocation ICON_HELP = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_help.png");
+	public static final ResourceLocation ICON_JAMMED = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_jammed.png");
+	public static final ResourceLocation ICON_NO_BOTTLES = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_noBottles.png");
+	public static final ResourceLocation ICON_NO_RECIPE = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_noRecipe.png");
+	public static final ResourceLocation ICON_NO_ENERGY = new ResourceLocation(Minechem.ID, Reference.ICON_BASE + "i_unpowered.png");
 
-    // Logging
-    public static final Logger LOGGER = LogManager.getLogger(Minechem.ID);
+	// Logging
+	public static final Logger LOGGER = LogManager.getLogger(Minechem.ID);
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        // Register instance.
-        INSTANCE = this;
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		// Register instance.
+		INSTANCE = this;
 
-        // Load configuration.
-        LOGGER.info("Loading configuration...");
-        Settings.init(event.getSuggestedConfigurationFile());
-        FMLCommonHandler.instance().bus().register(new Settings());
+		// Load configuration.
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Loading configuration...");
+		}
+		Settings.init(event.getSuggestedConfigurationFile());
+		FMLCommonHandler.instance().bus().register(new Settings());
 
-        LOGGER.info("Registering Packets...");
-        MessageHandler.init();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Packets...");
+		}
+		MessageHandler.init();
 
-        // Setup Mod Metadata for players to see in mod list with other mods.
-        metadata.modId = Minechem.ID;
-        metadata.name = Minechem.NAME;
-        metadata.description = Minechem.NAME + " is a mod about chemistry, allowing you to research blocks and items, and then break them down into their base compounds and elements.";
-        metadata.url = "http://www.minechemmod.com/";
-        metadata.logoFile = "assets/" + Minechem.ID + "/logo.png";
-        metadata.version = V_MAJOR + "." + V_MINOR + "." + V_REVIS;
-        metadata.authorList = Arrays.asList(new String[]
-        {
-            "pixlepix", "jakimfett", "maxwolf"
-        });
-        metadata.credits = "View a full list of contributors on our site!";
-        metadata.autogenerated = false;
+		// Setup Mod Metadata for players to see in mod list with other mods.
+		metadata.modId = Minechem.ID;
+		metadata.name = Minechem.NAME;
+		metadata.description = Minechem.NAME + " is a mod about chemistry, allowing you to research blocks and items, and then break them down into their base compounds and elements.";
+		metadata.url = "http://www.minechemmod.com/";
+		metadata.logoFile = "assets/" + Minechem.ID + "/logo.png";
+		metadata.version = V_MAJOR + "." + V_MINOR + "." + V_REVIS;
+		metadata.authorList = Arrays.asList(new String[]
+		{
+			"pixlepix", "jakimfett", "maxwolf"
+		});
+		metadata.credits = "View a full list of contributors on our site!";
+		metadata.autogenerated = false;
 
-        // Register items and blocks.
-        LOGGER.info("Registering Items...");
-        MinechemItemsRegistration.init();
+		// Register items and blocks.
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Items...");
+		}
+		MinechemItemsRegistration.init();
 
-        LOGGER.info("Registering Blocks...");
-        MinechemBlocksGeneration.registerBlocks();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Blocks...");
+		}
+		MinechemBlocksGeneration.registerBlocks();
 
-        LOGGER.info("Registering Blueprints...");
-        MinechemBlueprint.registerBlueprints();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Blueprints...");
+		}
+		MinechemBlueprint.registerBlueprints();
 
-        LOGGER.info("Registering Recipe Handlers...");
-        MinechemRecipes.getInstance().RegisterHandlers();
-        MinechemRecipes.getInstance().RegisterRecipes();
-        MinechemRecipes.getInstance().registerFluidRecipies();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Recipe Handlers...");
+		}
+		MinechemRecipes.getInstance().RegisterHandlers();
+		MinechemRecipes.getInstance().RegisterRecipes();
+		MinechemRecipes.getInstance().registerFluidRecipies();
 
-        LOGGER.info("Registering OreDict Compatability...");
-        MinechemItemsRegistration.registerToOreDictionary();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering OreDict Compatability...");
+		}
+		MinechemItemsRegistration.registerToOreDictionary();
 
-        LOGGER.info("Registering Minechem Recipes...");
-        MinecraftForge.EVENT_BUS.register(MinechemRecipes.getInstance());
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Minechem Recipes...");
+		}
+		MinecraftForge.EVENT_BUS.register(MinechemRecipes.getInstance());
 
-        LOGGER.info("Registering Chemical Effects...");
-        MinecraftForge.EVENT_BUS.register(new PotionCoatingSubscribe());
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Chemical Effects...");
+		}
+		MinecraftForge.EVENT_BUS.register(new PotionCoatingSubscribe());
 
-        LOGGER.info("Registering Polytool Event Handler...");
-        MinecraftForge.EVENT_BUS.register(new PolytoolEventHandler());
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Polytool Event Handler...");
+		}
+		MinecraftForge.EVENT_BUS.register(new PolytoolEventHandler());
+	}
 
-        LOGGER.info("PREINT PASSED");
-    }
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Proxy Hooks...");
+		}
+		PROXY.registerHooks();
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        LOGGER.info("Registering Proxy Hooks...");
-        PROXY.registerHooks();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Activating Potion Injector...");
+		}
+		PotionInjector.inject();
 
-        LOGGER.info("Activating Potion Injector...");
-        PotionInjector.inject();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Matching Pharmacology Effects to Chemicals...");
+		}
+		CraftingManager.getInstance().getRecipeList().add(new PotionCoatingRecipe());
 
-        LOGGER.info("Matching Pharmacology Effects to Chemicals...");
-        CraftingManager.getInstance().getRecipeList().add(new PotionCoatingRecipe());
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering fluids...");
+		}
+		FluidHelper.registerFluids();
 
-        LOGGER.info("Registering fluids...");
-        FluidHelper.registerFluids();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering Ore Generation...");
+		}
+		GameRegistry.registerWorldGenerator(new MinechemGeneration(), 0);
 
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering GUI and Container handlers...");
+		}
+		NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
 
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Register Tick Events for chemical effects tracking...");
+		}
+		PROXY.registerTickHandlers();
 
-        LOGGER.info("Registering Ore Generation...");
-        GameRegistry.registerWorldGenerator(new MinechemGeneration(), 0);
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Registering ClientProxy Rendering Hooks...");
+		}
+		PROXY.registerRenderers();
+	}
 
-        LOGGER.info("Registering GUI and Container handlers...");
-        NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		// Adds blueprints to dungeon loot.
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Adding blueprints to dungeon loot...");
+		}
+		addDungeonLoot();
 
-        LOGGER.info("Register Tick Events for chemical effects tracking...");
-        PROXY.registerTickHandlers();
+		if (Settings.DebugMode)
+		{
+			LOGGER.info("Activating Chemical Effect Layering (Coatings)...");
+		}
+		PotionEnchantmentCoated.registerCoatings();
+		
+		LOGGER.info("Minechem has loaded");
+	}
 
-        LOGGER.info("Registering ClientProxy Rendering Hooks...");
-        PROXY.registerRenderers();
+	private void addDungeonLoot()
+	{
+		ChestGenHooks ChestProvider = ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST);
+		ItemStack A = new ItemStack(MinechemItemsRegistration.blueprint, 1, 0);
+		ItemStack B = new ItemStack(MinechemItemsRegistration.blueprint, 1, 1);
+		ChestProvider.addItem(new WeightedRandomChestContent(A, 10, 80, 1));
+		ChestProvider.addItem(new WeightedRandomChestContent(B, 10, 80, 1));
+	}
 
-        LOGGER.info("INIT PASSED");
-    }
+	@SideOnly(Side.CLIENT)
+	public void textureHook(IIconRegister icon)
+	{
+		GuiTabHelp.helpIcon = icon.registerIcon(Reference.HELP_ICON);
+		GuiTabTable.helpIcon = icon.registerIcon(Reference.HELP_ICON);
+		ChemistJournalTab.helpIcon = icon.registerIcon(Reference.POWER_ICON);
+	}
 
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-        // Adds blueprints to dungeon loot.
-        LOGGER.info("Adding blueprints to dungeon loot...");
-        addDungeonLoot();
-
-        LOGGER.info("Activating Chemical Effect Layering (Coatings)...");
-        PotionEnchantmentCoated.registerCoatings();
-
-        LOGGER.info("POSTINIT PASSED");
-    }
-
-    private void addDungeonLoot()
-    {
-        ChestGenHooks ChestProvider = ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST);
-        ItemStack A = new ItemStack(MinechemItemsRegistration.blueprint, 1, 0);
-        ItemStack B = new ItemStack(MinechemItemsRegistration.blueprint, 1, 1);
-        ChestProvider.addItem(new WeightedRandomChestContent(A, 10, 80, 1));
-        ChestProvider.addItem(new WeightedRandomChestContent(B, 10, 80, 1));
-    }
-
-    @SideOnly(Side.CLIENT)
-    public void textureHook(IIconRegister icon)
-    {
-        GuiTabHelp.helpIcon = icon.registerIcon(Reference.HELP_ICON);
-        GuiTabTable.helpIcon = icon.registerIcon(Reference.HELP_ICON);
-        ChemistJournalTab.helpIcon = icon.registerIcon(Reference.POWER_ICON);
-    }
-
-    @SubscribeEvent
-    public void onPreRender(RenderGameOverlayEvent.Pre e)
-    {
-        TickHandler.renderEffects();
-    }
+	@SubscribeEvent
+	public void onPreRender(RenderGameOverlayEvent.Pre e)
+	{
+		TickHandler.renderEffects();
+	}
 }
--- src/main/java/minechem/oredictionary/OreDictionaryDefaultHandler.java
+++ src/main/java/minechem/oredictionary/OreDictionaryDefaultHandler.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.logging.Level;
 
 import minechem.Minechem;
+import minechem.Settings;
 import minechem.potion.PotionChemical;
 import minechem.tileentity.decomposer.DecomposerRecipe;
 import minechem.tileentity.synthesis.SynthesisRecipe;
@@ -16,157 +17,175 @@ import net.minecraftforge.oredict.OreDictionary.OreRegisterEvent;
 public class OreDictionaryDefaultHandler implements OreDictionaryHandler
 {
 
-    private enum EnumOrePrefix
-    {
-        dust, block, ingot, ore, dustSmall, nugget, dustDirty, plate, gem
-    }
+	private enum EnumOrePrefix
+	{
+		dust, block, ingot, ore, dustSmall, nugget, dustDirty, plate, gem
+	}
 
-    private String[] supportedOres;
+	private String[] supportedOres;
 
-    private Map<OreDictionaryBaseOreEnum, ArrayList<EnumOrePrefix>> seenOres = new HashMap<OreDictionaryBaseOreEnum, ArrayList<EnumOrePrefix>>();
+	private Map<OreDictionaryBaseOreEnum, ArrayList<EnumOrePrefix>> seenOres = new HashMap<OreDictionaryBaseOreEnum, ArrayList<EnumOrePrefix>>();
 
-    private Map<OreDictionaryBaseOreEnum, ItemStack> registeredIngots = new HashMap<OreDictionaryBaseOreEnum, ItemStack>();
+	private Map<OreDictionaryBaseOreEnum, ItemStack> registeredIngots = new HashMap<OreDictionaryBaseOreEnum, ItemStack>();
 
-    public OreDictionaryDefaultHandler()
-    {
-        ArrayList<String> ores = new ArrayList<String>();
-        for (OreDictionaryBaseOreEnum ore : OreDictionaryBaseOreEnum.values())
-        {
-            ores.add(ore.name());
-        }
-        supportedOres = ores.toArray(new String[ores.size()]);
-    }
+	public OreDictionaryDefaultHandler()
+	{
+		ArrayList<String> ores = new ArrayList<String>();
+		for (OreDictionaryBaseOreEnum ore : OreDictionaryBaseOreEnum.values())
+		{
+			ores.add(ore.name());
+		}
+		supportedOres = ores.toArray(new String[ores.size()]);
+	}
 
-    public String[] parseOreName(String oreName)
-    {
-        for (EnumOrePrefix prefix : EnumOrePrefix.values())
-        {
-            if (oreName.startsWith(prefix.name()))
-            {
-                String remainder = oreName.substring(prefix.name().length()).toLowerCase();
-                if (Arrays.asList(supportedOres).contains(remainder))
-                    return new String[]
-                    { prefix.name(), remainder };
-            }
-        }
+	public String[] parseOreName(String oreName)
+	{
+		for (EnumOrePrefix prefix : EnumOrePrefix.values())
+		{
+			if (oreName.startsWith(prefix.name()))
+			{
+				String remainder = oreName.substring(prefix.name().length()).toLowerCase();
+				if (Arrays.asList(supportedOres).contains(remainder))
+				{
+					return new String[]
+					{
+						prefix.name(), remainder
+					};
+				}
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    @Override
-    public boolean canHandle(OreRegisterEvent event)
-    {
-        if (this.parseOreName(event.Name) != null)
-            return true;
-        return false;
-    }
+	@Override
+	public boolean canHandle(OreRegisterEvent event)
+	{
+		if (this.parseOreName(event.Name) != null)
+		{
+			return true;
+		}
+		return false;
+	}
 
-    @Override
-    public void handle(OreRegisterEvent event)
-    {
-        Minechem.LOGGER.info(OreDictionaryDefaultHandler.class.getSimpleName() + " registered : " + event.Name);
+	@Override
+	public void handle(OreRegisterEvent event)
+	{
+		if (Settings.DebugMode)
+		{
+			Minechem.LOGGER.info(OreDictionaryDefaultHandler.class.getSimpleName() + " registered : " + event.Name);
+		}
 
-        String[] tokens = this.parseOreName(event.Name);
-        EnumOrePrefix prefix = EnumOrePrefix.valueOf(tokens[0]);
-        OreDictionaryBaseOreEnum ore = OreDictionaryBaseOreEnum.valueOf(tokens[1]);
+		String[] tokens = this.parseOreName(event.Name);
+		EnumOrePrefix prefix = EnumOrePrefix.valueOf(tokens[0]);
+		OreDictionaryBaseOreEnum ore = OreDictionaryBaseOreEnum.valueOf(tokens[1]);
 
-        switch (prefix)
-        {
-        case ore:
-            DecomposerRecipe.add(new DecomposerRecipe(event.Ore, scaleFloor(ore.getComposition(), 3d)));
+		switch (prefix)
+		{
+			case ore:
+				DecomposerRecipe.add(new DecomposerRecipe(event.Ore, scaleFloor(ore.getComposition(), 3d)));
             // Removed to prevent dupes with RC
-            // SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, scaleFloor(ore.getComposition(),3d)));
-            break;
-        case ingot:
-            DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
-            if (!haveSeen(ore, EnumOrePrefix.dust) && !haveSeen(ore, EnumOrePrefix.dustSmall))
-            {
-                SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, ore.getComposition()));
-                registeredIngots.put(ore, event.Ore);
-            }
-            break;
+				// SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, scaleFloor(ore.getComposition(),3d)));
+				break;
+			case ingot:
+				DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
+				if (!haveSeen(ore, EnumOrePrefix.dust) && !haveSeen(ore, EnumOrePrefix.dustSmall))
+				{
+					SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, ore.getComposition()));
+					registeredIngots.put(ore, event.Ore);
+				}
+				break;
 
-        case nugget:
-            DecomposerRecipe.add(new DecomposerRecipe(event.Ore, scaleFloor(ore.getComposition(), 1d / 9d)));
-            break;
-        case dust:
-            DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
-            unregisterIngot(ore);
-            SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, ore.getComposition()));
-            break;
-        case dustDirty:
-            DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
-            break;
-        case plate:
-            DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
-            break;
-        case dustSmall:
-            DecomposerRecipe.add(new DecomposerRecipe(event.Ore, scaleFloor(ore.getComposition(), 0.25d)));
-            unregisterIngot(ore);
-            SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, scaleCeil(ore.getComposition(), 0.25d)));
-            break;
-        case gem:
-            DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
-            SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, ore.getComposition()));
-            break;
+			case nugget:
+				DecomposerRecipe.add(new DecomposerRecipe(event.Ore, scaleFloor(ore.getComposition(), 1d / 9d)));
+				break;
+			case dust:
+				DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
+				unregisterIngot(ore);
+				SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, ore.getComposition()));
+				break;
+			case dustDirty:
+				DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
+				break;
+			case plate:
+				DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
+				break;
+			case dustSmall:
+				DecomposerRecipe.add(new DecomposerRecipe(event.Ore, scaleFloor(ore.getComposition(), 0.25d)));
+				unregisterIngot(ore);
+				SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, scaleCeil(ore.getComposition(), 0.25d)));
+				break;
+			case gem:
+				DecomposerRecipe.add(new DecomposerRecipe(event.Ore, ore.getComposition()));
+				SynthesisRecipe.add(new SynthesisRecipe(event.Ore, false, 1000, ore.getComposition()));
+				break;
 
-        default:
-            Minechem.LOGGER.warn(OreDictionaryDefaultHandler.class.getSimpleName() + " : Invalid ore dictionary type.");
-            break;
-        }
+			default:
+				if (Settings.DebugMode)
+				{
+					Minechem.LOGGER.warn(OreDictionaryDefaultHandler.class.getSimpleName() + " : Invalid ore dictionary type.");
+				}
+				break;
+		}
 
-        seen(ore, prefix);
-    }
+		seen(ore, prefix);
+	}
 
-    private void unregisterIngot(OreDictionaryBaseOreEnum ore)
-    {
-        if (registeredIngots.containsKey(ore))
-        {
-            SynthesisRecipe.remove(registeredIngots.get(ore));
-            registeredIngots.remove(ore);
-        }
-    }
+	private void unregisterIngot(OreDictionaryBaseOreEnum ore)
+	{
+		if (registeredIngots.containsKey(ore))
+		{
+			SynthesisRecipe.remove(registeredIngots.get(ore));
+			registeredIngots.remove(ore);
+		}
+	}
 
-    private PotionChemical[] scaleCeil(PotionChemical[] composition, double factor)
-    {
-        ArrayList<PotionChemical> newComposition = new ArrayList<PotionChemical>();
+	private PotionChemical[] scaleCeil(PotionChemical[] composition, double factor)
+	{
+		ArrayList<PotionChemical> newComposition = new ArrayList<PotionChemical>();
 
-        for (PotionChemical chem : composition)
-        {
-            PotionChemical newChem = chem.copy();
-            newChem.amount = (int) Math.ceil(chem.amount * factor);
-            newComposition.add(newChem);
-        }
+		for (PotionChemical chem : composition)
+		{
+			PotionChemical newChem = chem.copy();
+			newChem.amount = (int) Math.ceil(chem.amount * factor);
+			newComposition.add(newChem);
+		}
 
-        return newComposition.toArray(new PotionChemical[newComposition.size()]);
-    }
+		return newComposition.toArray(new PotionChemical[newComposition.size()]);
+	}
 
-    private PotionChemical[] scaleFloor(PotionChemical[] composition, double factor)
-    {
-        ArrayList<PotionChemical> newComposition = new ArrayList<PotionChemical>();
+	private PotionChemical[] scaleFloor(PotionChemical[] composition, double factor)
+	{
+		ArrayList<PotionChemical> newComposition = new ArrayList<PotionChemical>();
 
-        for (PotionChemical chem : composition)
-        {
-            PotionChemical newChem = chem.copy();
-            newChem.amount = (int) Math.floor(chem.amount * factor);
-            newComposition.add(newChem);
-        }
+		for (PotionChemical chem : composition)
+		{
+			PotionChemical newChem = chem.copy();
+			newChem.amount = (int) Math.floor(chem.amount * factor);
+			newComposition.add(newChem);
+		}
 
-        return newComposition.toArray(new PotionChemical[newComposition.size()]);
-    }
+		return newComposition.toArray(new PotionChemical[newComposition.size()]);
+	}
 
-    private boolean haveSeen(OreDictionaryBaseOreEnum ore, EnumOrePrefix prefix)
-    {
-        if (this.seenOres.containsKey(ore) && this.seenOres.get(ore).contains(prefix))
-            return true;
-        return false;
-    }
+	private boolean haveSeen(OreDictionaryBaseOreEnum ore, EnumOrePrefix prefix)
+	{
+		if (this.seenOres.containsKey(ore) && this.seenOres.get(ore).contains(prefix))
+		{
+			return true;
+		}
+		return false;
+	}
 
-    private void seen(OreDictionaryBaseOreEnum ore, EnumOrePrefix prefix)
-    {
-        if (!this.seenOres.containsKey(ore))
-            this.seenOres.put(ore, new ArrayList<EnumOrePrefix>());
-        if (!this.seenOres.get(ore).contains(prefix))
-            this.seenOres.get(ore).add(prefix);
-    }
+	private void seen(OreDictionaryBaseOreEnum ore, EnumOrePrefix prefix)
+	{
+		if (!this.seenOres.containsKey(ore))
+		{
+			this.seenOres.put(ore, new ArrayList<EnumOrePrefix>());
+		}
+		if (!this.seenOres.get(ore).contains(prefix))
+		{
+			this.seenOres.get(ore).add(prefix);
+		}
+	}
 }
