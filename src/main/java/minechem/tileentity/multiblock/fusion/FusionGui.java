c05a37c0223bc79be3ed68123f3f6df2a6e25ff7
jakimfett <jakimfett@picraft.com>
2014-10-04 21:12:00 -0700

    GUI Energy bar fix

--- src/main/java/minechem/tileentity/multiblock/fusion/FusionGui.java
+++ src/main/java/minechem/tileentity/multiblock/fusion/FusionGui.java
@@ -10,86 +10,80 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-
 import org.lwjgl.opengl.GL11;
 
 public class FusionGui extends GuiContainerTabbed
 {
 
-    static float increaseRate = .2F;
-    static float decreaseRate = .4F;
+	static float increaseRate = .2F;
+	static float decreaseRate = .4F;
 
-    FusionTileEntity fusion;
-    int guiWidth = 176;
-    int guiHeight = 187;
-    int targetEnergy = 0;
+	FusionTileEntity fusion;
+	static int guiWidth = 176;
+	static int guiHeight = 187;
+	int storedEnergy;
+	int maxEnergy;
 
-    public FusionGui(InventoryPlayer inventoryPlayer, FusionTileEntity fusion)
-    {
-        super(new FusionContainer(inventoryPlayer, fusion));
-        this.fusion = fusion;
-        this.xSize = guiWidth;
-        this.ySize = guiHeight;
-        addTab(new GuiTabHelp(this, MinechemHelper.getLocalString("help.fusion")));
-        addTab(new FusionTabStateControl(this, fusion));
-  }
+	public FusionGui(InventoryPlayer inventoryPlayer, FusionTileEntity fusion)
+	{
+		super(new FusionContainer(inventoryPlayer, fusion));
+		this.fusion = fusion;
+		this.xSize = guiWidth;
+		this.ySize = guiHeight;
+		this.storedEnergy = this.fusion.getEnergyStored();
+		this.maxEnergy = fusion.getMaxEnergyStored();
+		addTab(new GuiTabHelp(this, MinechemHelper.getLocalString("help.fusion")));
+		addTab(new FusionTabStateControl(this, fusion));
+	}
 
-    @Override
-    protected void drawGuiContainerForegroundLayer(int par1, int par2)
-    {
-        super.drawGuiContainerForegroundLayer(par1, par2);
-        String info = MinechemHelper.getLocalString("block.fusionReactor.name");
-        int infoWidth = fontRendererObj.getStringWidth(info);
-        fontRendererObj.drawString(info, (guiWidth - infoWidth) / 2, 5, 0x000000);
-    }
+	@Override
+	protected void drawGuiContainerForegroundLayer(int par1, int par2)
+	{
+		super.drawGuiContainerForegroundLayer(par1, par2);
+		String info = MinechemHelper.getLocalString("block.fusionReactor.name");
+		int infoWidth = fontRendererObj.getStringWidth(info);
+		fontRendererObj.drawString(info, (guiWidth - infoWidth) / 2, 5, 0x000000);
+	}
 
-    @Override
-    protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3)
-    {
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3)
+	{
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		updateEnergy();
 
-        this.mc.renderEngine.bindTexture(new ResourceLocation(Minechem.ID, Reference.FUSION_GUI));
-        int x = (width - guiWidth) / 2;
-        int y = (height - guiHeight) / 2;
-        // DRAW GUI
-        drawTexturedModalRect(x, y, 0, 0, guiWidth, guiHeight);
+		this.mc.renderEngine.bindTexture(new ResourceLocation(Minechem.ID, Reference.FUSION_GUI));
+		int x = (width - guiWidth) / 2;
+		int y = (height - guiHeight) / 2;
+		// DRAW GUI
+		drawTexturedModalRect(x, y, 0, 0, guiWidth, guiHeight);
 
-        // DRAW ENERGY BAR
-        updateEnergy();
+		// DRAW ENERGY BAR OVERLAY
+		drawEnergyBarOverlay(x, y);
+	}
 
-        // @TODO - calculate energybar width based on machine state and energy
-        drawTexturedModalRect(x + 8, y + 38, 0, 192, 10, 3);
+	public void drawEnergyBarOverlay(int x, int y)
+	{
+		// @TODO - calculate energybar width based on machine state and energy
+		drawTexturedModalRect(x + 8, y + 38, 0, 187, this.fusion.getPowerRemainingScaled(160), 3);
 
-        // DRAW ENERGY BAR OVERLAY
-        drawEnergyBarOverlay();
-    }
+	}
 
-    public void drawEnergyBarOverlay()
-    {
+	private void updateEnergy()
+	{
+		this.storedEnergy = this.fusion.getEnergyStored();
+	}
 
-        int x = (width - guiWidth) / 2;
-        int y = (height - guiHeight) / 2;
-        drawTexturedModalRect(x + 7, y + 37, 0, 187, 162, 5);
+	@Override
+	public boolean hideItemPanelSlot(GuiContainer gui, int x, int y, int w, int h)
+	{
+		return false;
+	}
 
-    }
-
-    private void updateEnergy()
-    {
-        targetEnergy = FusionTileEntity.output;
-        // @TODO - calculate energy 
-    }
-
-    @Override
-    public boolean hideItemPanelSlot(GuiContainer gui, int x, int y, int w, int h)
-    {
-        return false;
-    }
-
-    @Optional.Method(modid = "NotEnoughItems")
-    @Override
-    public Iterable<Integer> getItemSpawnSlots(GuiContainer gc, ItemStack is)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Optional.Method(modid = "NotEnoughItems")
+	@Override
+	public Iterable<Integer> getItemSpawnSlots(GuiContainer gc, ItemStack is)
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
 
 }
