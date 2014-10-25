0adbfd796b6ee531a8e53e9ccdea56140f9a0a3b
widd <widd@teamimmersion.net>
2014-10-25 02:10:01 -0700

    Changed iron bar value to 6 Fe

--- src/main/java/minechem/MinechemRecipes.java
+++ src/main/java/minechem/MinechemRecipes.java
@@ -2494,13 +2494,13 @@ public class MinechemRecipes
 		ItemStack bars = new ItemStack(Blocks.iron_bars);
 		DecomposerRecipe.add(new DecomposerRecipe(bars, new PotionChemical[]
 		{
-			element(ElementEnum.Fe, 8),
-            element(ElementEnum.Fe, 8)
+			element(ElementEnum.Fe, 3),
+            element(ElementEnum.Fe, 3)
 		}));
 		SynthesisRecipe.add(new SynthesisRecipe(bars, false, COST_BLOCK, new PotionChemical[]
 		{
-                element(ElementEnum.Fe, 8),
-                element(ElementEnum.Fe, 8)
+                element(ElementEnum.Fe, 3),
+                element(ElementEnum.Fe, 3)
 		}));
 	}
 
