7f86c69708fc5c44054c0de18160cc52ff5723d4
Yves <Mazdallier@users.noreply.github.com>
2014-11-13 14:31:29 +0100

    Update OreDictionaryMekanismHandler.java
    As a begining. i would like to add Brine Bucket, Saw dust, Obsidian ingot and Glowstone ingot...
    Somme recepes in head.

--- src/main/java/minechem/oredictionary/OreDictionaryMekanismHandler.java
+++ src/main/java/minechem/oredictionary/OreDictionaryMekanismHandler.java
@@ -1,18 +1,66 @@
 package minechem.oredictionary;
 
+import minechem.item.molecule.Molecule;
+import minechem.item.molecule.MoleculeEnum;
+import minechem.potion.PotionChemical;
+import minechem.tileentity.decomposer.DecomposerRecipe;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class OreDictionaryMekanismHandler implements OreDictionaryHandler
 {
+
+	private static final int OSNIUM = 0;
+	private static final int COPPER = 1;
+	private static final int TIN = 2;
+	private static final int BRONZE = 3;
+	private static final int STEEL = 4;
+
+	private String[] blockTypes = new String[]
+	{
+		"OsniumOre", "CopperOre", "TinOre", "Bronze", "Steel"
+	};
+
+	private List<String> blockList = Arrays.asList(blockTypes);
 
 	@Override
 	public boolean canHandle(String oreName)
 	{
-		// TODO Auto-generated method stub
-		return false;
+		return blockList.contains(oreName);
 	}
 
 	@Override
 	public void handle(String oreName)
 	{
-		// TODO Auto-generated method stub
+		switch (blockList.indexOf(oreName))
+		{
+			case OSNIUM
+				DecomposerRecipe.createAndAddRecipeSafely(oreName, new PotionChemical[]
+				{
+					new Element(ElementEnum.Os, 16)
+				});
+			case COPPER
+				DecomposerRecipe.createAndAddRecipeSafely(oreName, new PotionChemical[]
+				{
+					new Element(ElementEnum.Cu, 16)
+				});
+			case TIN
+				DecomposerRecipe.createAndAddRecipeSafely(oreName, new PotionChemical[]
+				{
+					new Element(ElementEnum.Sn, 16)
+				});
+			case BRONZE
+				DecomposerRecipe.createAndAddRecipeSafely(oreName, new PotionChemical[]
+				{
+					new Element(ElementEnum.Cu, 12), new Element(ElementEnum.Sn, 4)
+				});
+			case STEEL
+				DecomposerRecipe.createAndAddRecipeSafely(oreName, new PotionChemical[]
+				{
+					new Element(ElementEnum.Fe, 14), new Element(ElementEnum.C, 2)
+				});
+		}
 	}
+
 }
