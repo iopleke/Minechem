e59de0073e87e403c214afcf058ee82b40c502a4
jakimfett <jakimfett@gmail.com>
2015-01-07 15:10:11 -0800

    Clean API test API version replacement

--- src/main/java/minechem/api/package-info.java
+++ src/main/java/minechem/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "minechem", provides = "minechemAPI", apiVersion = "1.7.10R1")
+@API(owner = "minechem", provides = "minechemAPI", apiVersion = "1.7.10v6.0.@BUILD@")
 package minechem.api;
 
 import cpw.mods.fml.common.API;
