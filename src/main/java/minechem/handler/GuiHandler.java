b244e4552c6a736d188b2aff6d0bd229d677987c
jakimfett <jakimfett@gmail.com>
2015-01-07 17:11:49 -0800

    Add javadocs to classes

--- src/main/java/minechem/handler/GuiHandler.java
+++ src/main/java/minechem/handler/GuiHandler.java
@@ -7,25 +7,47 @@ import net.minecraft.world.World;
 
 public class GuiHandler implements IGuiHandler
 {
-    @Override
-    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-    {
-        TileEntity tileEntity = world.getTileEntity(x, y, z);
-        if (tileEntity != null)
-        {
-            // use instanceof to return the correct container object
-        }
-        return null;
-    }
+	/**
+	 * Get the GUI container object for the server
+	 *
+	 * @param ID     GUI element ID, unused
+	 * @param player Player entity
+	 * @param world  World object
+	 * @param x      World x coordinate
+	 * @param y      World y coordinate
+	 * @param z      World z coordinate
+	 * @return Container object for the TileEntity
+	 */
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+	{
+		TileEntity tileEntity = world.getTileEntity(x, y, z);
+		if (tileEntity != null)
+		{
+			// use instanceof to return the correct container object
+		}
+		return null;
+	}
 
-    @Override
-    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-    {
-        TileEntity tileEntity = world.getTileEntity(x, y, z);
-        if (tileEntity != null)
-        {
-            // use instanceof to return the correct GUI object
-        }
-        return null;
-    }
+	/**
+	 * Get the GUI container object for the server
+	 *
+	 * @param ID     GUI element ID, unused
+	 * @param player Player entity
+	 * @param world  World object
+	 * @param x      World x coordinate
+	 * @param y      World y coordinate
+	 * @param z      World z coordinate
+	 * @return GUI object for the TileEntity
+	 */
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+	{
+		TileEntity tileEntity = world.getTileEntity(x, y, z);
+		if (tileEntity != null)
+		{
+			// use instanceof to return the correct GUI object
+		}
+		return null;
+	}
 }
--- src/main/java/minechem/handler/MessageHandler.java
+++ src/main/java/minechem/handler/MessageHandler.java
@@ -8,16 +8,25 @@ import minechem.reference.Compendium;
 
 public class MessageHandler implements IMessageHandler
 {
-    public static SimpleNetworkWrapper INSTANCE = new SimpleNetworkWrapper(Compendium.Naming.id);
-    public static int id = 0;
+	public static SimpleNetworkWrapper INSTANCE = new SimpleNetworkWrapper(Compendium.Naming.id);
 
-    public static void init()
-    {
-    }
+	/**
+	 * Initialize the MessageHandler
+	 */
+	public static void init()
+	{
+	}
 
-    @Override
-    public IMessage onMessage(IMessage message, MessageContext ctx)
-    {
-        return null;
-    }
+	/**
+	 * Called when a message is received of the appropriate type. You can
+	 * optionally return a reply message, or null if no reply is needed.
+	 *
+	 * @param message The message
+	 * @return an optional return message
+	 */
+	@Override
+	public IMessage onMessage(IMessage message, MessageContext ctx)
+	{
+		return null;
+	}
 }
