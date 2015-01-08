a5c8f0561d4c7b8c3a24899e67b2d3607a2231db
hilburn <charliepaterson89@gmail.com>
2015-01-08 22:07:41 +0000

    Adds Valence Shell Calculation

--- src/main/java/minechem/element/Element.java
+++ src/main/java/minechem/element/Element.java
@@ -1,10 +1,17 @@
 package minechem.element;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Data object for elements
  */
 public class Element
 {
+	private static final Matcher SHELL_ORDER = Pattern.compile("\\d+([a-z])").matcher("1s2s2p3s3p4s3d4p5s4d5p6s4f5d6p7s5f6d7p8s5g6f7d8p9s"); //Handles up to atomic number 170
+	private static final int[] SUB_SHELL_ELECTRONS = new int[]{2,6,10,14,18};
+	private static final String SUB_SHELL_STRING = "spdfg";																					//Super Powered Dog Fights God
+
 	public final int atomicNumber;
 	public final String fullName;
 	public final String shortName;
@@ -70,10 +77,22 @@ public class Element
 		return this.valenceSubshellIdentifier;
 	}
 
+	/**
+	 * Iterates over the {@code SHELL_ORDER} string, calculates the size of the sub-shell from it's type
+	 * and subtracts it from the electron count. when that is count <= 0 the latest sub-shell is the valence shell
+	 * and full shell electrons + the count (which is normally -ve) gives the valence electrons.
+	 * Can easily be modified to recover the Shell no. separately to the full "1s" value by changing the SHELL_ORDER pattern to:
+	 * (//d+)([a-z]) and the casting group(1) to an int, and using group(2) to find the sub-shell type
+	 */
 	private void calculateValenceShells()
 	{
-		// do the calculations for the valence shell values here, @hilburn
-		// then use the setValenceShell method to save the values
+		SHELL_ORDER.reset();
+		int electronCount = atomicNumber, subShell = 0;
+		while (electronCount>0 && SHELL_ORDER.find())
+		{
+			subShell = SUB_SHELL_STRING.indexOf(SHELL_ORDER.group(1));
+			electronCount-= SUB_SHELL_ELECTRONS[subShell];
+		}
+		setValenceShell(SUB_SHELL_ELECTRONS[subShell] + electronCount, SHELL_ORDER.group(0));
 	}
-
 }
