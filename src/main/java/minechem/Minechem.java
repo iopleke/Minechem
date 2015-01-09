e942b338d6721268919c8abb39aca854072fb7c1
jakimfett <jakimfett@gmail.com>
2015-01-08 18:04:11 -0800

    Fix gui factory registration

--- src/main/java/minechem/Minechem.java
+++ src/main/java/minechem/Minechem.java
@@ -19,75 +19,75 @@ import minechem.registry.BlockRegistry;
 import minechem.registry.ItemRegistry;
 import minechem.registry.RecipeRegistry;
 
-@Mod(modid = Compendium.Naming.id, name = Compendium.Naming.name, version = Compendium.Version.full, useMetadata = false, guiFactory = "minechem.client.gui.GuiFactory", acceptedMinecraftVersions = "[1.7.10,)", dependencies = "required-after:Forge@[10.13.0.1180,)")
+@Mod(modid = Compendium.Naming.id, name = Compendium.Naming.name, version = Compendium.Version.full, useMetadata = false, guiFactory = "minechem.proxy.client.gui.GuiFactory", acceptedMinecraftVersions = "[1.7.10,)", dependencies = "required-after:Forge@[10.13.0.1180,)")
 public class Minechem
 {
-    public static boolean isCoFHAAPILoaded;
+	public static boolean isCoFHAAPILoaded;
 
-    // Instancing
-    @Instance(value = Compendium.Naming.id)
-    public static Minechem INSTANCE;
+	// Instancing
+	@Instance(value = Compendium.Naming.id)
+	public static Minechem INSTANCE;
 
-    // Public extra data about our mod that Forge uses in the mods listing page for more information.
-    @Mod.Metadata(Compendium.Naming.id)
-    public static ModMetadata metadata;
+	// Public extra data about our mod that Forge uses in the mods listing page for more information.
+	@Mod.Metadata(Compendium.Naming.id)
+	public static ModMetadata metadata;
 
-    @SidedProxy(clientSide = "minechem.proxy.client.ClientProxy", serverSide = "minechem.proxy.CommonProxy")
-    public static CommonProxy PROXY;
+	@SidedProxy(clientSide = "minechem.proxy.client.ClientProxy", serverSide = "minechem.proxy.CommonProxy")
+	public static CommonProxy PROXY;
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        // Register instance.
-        INSTANCE = this;
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		// Register instance.
+		INSTANCE = this;
 
-        try
-        {
-            // Shouldn't we be using Loader.isModLoaded here?
-            Class.forName("cofh.api.energy.IEnergyHandler");
-            isCoFHAAPILoaded = true;
-        } catch (Exception e)
-        {
-            isCoFHAAPILoaded = false;
-        }
+		try
+		{
+			// Shouldn't we be using Loader.isModLoaded here?
+			Class.forName("cofh.api.energy.IEnergyHandler");
+			isCoFHAAPILoaded = true;
+		} catch (Exception e)
+		{
+			isCoFHAAPILoaded = false;
+		}
 
-        // Load configuration.
-        LogHelper.debug("Loading configuration...");
-        Config.init(event.getSuggestedConfigurationFile());
-        FMLCommonHandler.instance().bus().register(new Config());
+		// Load configuration.
+		LogHelper.debug("Loading configuration...");
+		Config.init(event.getSuggestedConfigurationFile());
+		FMLCommonHandler.instance().bus().register(new Config());
 
-        LogHelper.debug("Registering Packets...");
-        MessageHandler.init();
+		LogHelper.debug("Registering Packets...");
+		MessageHandler.init();
 
-        LogHelper.debug("Setting up ModMetaData");
-        metadata = Compendium.MetaData.init(metadata);
+		LogHelper.debug("Setting up ModMetaData");
+		metadata = Compendium.MetaData.init(metadata);
 
-        // Register items and blocks.
-        LogHelper.debug("Registering Items...");
-        ItemRegistry.init();
+		// Register items and blocks.
+		LogHelper.debug("Registering Items...");
+		ItemRegistry.init();
 
-        LogHelper.debug("Registering Blocks...");
-        BlockRegistry.init();
+		LogHelper.debug("Registering Blocks...");
+		BlockRegistry.init();
 
-    }
+	}
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        LogHelper.debug("Registering GUI and Container handlers...");
-        NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		LogHelper.debug("Registering GUI and Container handlers...");
+		NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
 
-        LogHelper.debug("Registering ClientProxy Rendering Hooks...");
-        PROXY.registerRenderers();
+		LogHelper.debug("Registering ClientProxy Rendering Hooks...");
+		PROXY.registerRenderers();
 
-    }
+	}
 
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-        LogHelper.debug("Registering Recipes...");
-        RecipeRegistry.getInstance().init();
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		LogHelper.debug("Registering Recipes...");
+		RecipeRegistry.getInstance().init();
 
-        LogHelper.info("Minechem has loaded");
-    }
+		LogHelper.info("Minechem has loaded");
+	}
 }
