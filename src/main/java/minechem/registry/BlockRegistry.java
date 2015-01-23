0a8e5b939d55dc9c2522215db156e3378edf5f61
jakimfett <jakimfett@gmail.com>
2015-01-22 20:02:33 -0800

    Microscope block and TE registration

--- src/main/java/minechem/registry/BlockRegistry.java
+++ src/main/java/minechem/registry/BlockRegistry.java
@@ -1,8 +1,18 @@
 package minechem.registry;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import minechem.apparatus.tier1.opticalMicroscope.OpticalMicroscopeBlock;
+import minechem.apparatus.tier1.opticalMicroscope.OpticalMicroscopeTileEntity;
+import minechem.reference.Compendium;
+
 public class BlockRegistry
 {
+    public static OpticalMicroscopeBlock opticalMicroscope;
+
     public static void init()
     {
+        opticalMicroscope = new OpticalMicroscopeBlock();
+        GameRegistry.registerBlock(opticalMicroscope, opticalMicroscope.getUnlocalizedName());
+        GameRegistry.registerTileEntity(OpticalMicroscopeTileEntity.class, Compendium.Naming.opticalMicroscope + "TE");
     }
 }
