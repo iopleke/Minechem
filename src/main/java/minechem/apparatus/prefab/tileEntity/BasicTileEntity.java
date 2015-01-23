56f36675309f9acf5d8320413f8c8c5c4681bede
jakimfett <jakimfett@gmail.com>
2015-01-22 18:32:05 -0800

    Flesh out TileEntity prefab

--- src/main/java/minechem/apparatus/prefab/tileEntity/BasicTileEntity.java
+++ src/main/java/minechem/apparatus/prefab/tileEntity/BasicTileEntity.java
@@ -1,9 +1,146 @@
 package minechem.apparatus.prefab.tileEntity;
 
+import codechicken.lib.inventory.InventoryUtils;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+
 /**
  * Defines basic properties for TileEntities
  */
-public class BasicTileEntity
+public abstract class BasicTileEntity extends TileEntity implements IInventory
 {
+    private ItemStack[] inventory;
+
+    @Override
+    public void closeInventory()
+    {
+    }
+
+    /**
+     * Decrease the stack in a given slot by a given amount
+     *
+     * @param slot   the inventory slot
+     * @param amount the amount to decrease the stack by
+     * @return ItemStack the stack from the slot
+     */
+    @Override
+    public ItemStack decrStackSize(int slot, int amount)
+    {
+        return InventoryUtils.decrStackSize(this, slot, amount);
+    }
+
+    /**
+     * Get the inventory name
+     *
+     * @return String the unlocalized inventory name
+     */
+    @Override
+    public abstract String getInventoryName();
+
+    /**
+     * Get the stack size limit for the inventory, override if necessary
+     *
+     * @return 64
+     */
+    @Override
+    public int getInventoryStackLimit()
+    {
+        return 64;
+    }
+
+    /**
+     * Get the size of the inventory
+     *
+     * @return int size of the inventory object
+     */
+    @Override
+    public int getSizeInventory()
+    {
+        return inventory.length;
+    }
+
+    /**
+     * Get the item from a specific slot
+     *
+     * @param slot slot to get the item from
+     * @return the itemstack from the slot
+     */
+    @Override
+    public ItemStack getStackInSlot(int slot)
+    {
+        return inventory[slot];
+    }
+
+    /**
+     * Get the stack in a given slot on GUI close
+     *
+     * @param slot the slot to get from
+     * @return ItemStack the stack from the slot
+     */
+    @Override
+    public ItemStack getStackInSlotOnClosing(int slot)
+    {
+        return InventoryUtils.getStackInSlotOnClosing(this, slot);
+    }
+
+    /**
+     * Check if the inventory has a custom name
+     *
+     * @return false
+     */
+    @Override
+    public boolean hasCustomInventoryName()
+    {
+        return false;
+    }
+
+    /**
+     * Can an item be put into the slot
+     *
+     * @param slot  slot to check
+     * @param stack itemstack to check
+     * @return true
+     */
+    @Override
+    public boolean isItemValidForSlot(int slot, ItemStack stack)
+    {
+        return true;
+    }
+
+    /**
+     * Check if the player can use the inventory
+     *
+     * @param entityPlayer the player object
+     * @return boolean based on distance and tileEntity status
+     */
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer entityPlayer)
+    {
+        double dist = entityPlayer.getDistanceSq(xCoord + 0.5D, yCoord + 0.5D, zCoord + 0.5D);
+        return worldObj.getTileEntity(xCoord, yCoord, zCoord) != this ? false : dist <= 64.0D;
+    }
+
+    @Override
+    public void openInventory()
+    {
+    }
+
+    /**
+     * Set the inventory slot to a given itemstack
+     *
+     * @param slot  which slot should the itemstack go into
+     * @param stack the stack to put into the slot
+     */
+    @Override
+    public void setInventorySlotContents(int slot, ItemStack stack)
+    {
+        inventory[slot] = stack;
+        markDirty();
+    }
+
+    @Override
+    public abstract void updateEntity();
 
 }
