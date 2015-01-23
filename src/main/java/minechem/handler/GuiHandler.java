7b535ebdafa87efded5d9f7a3d198a65a1b9616a
jakimfett <jakimfett@gmail.com>
2015-01-22 20:00:53 -0800

    Add gui checks for microscope

--- src/main/java/minechem/handler/GuiHandler.java
+++ src/main/java/minechem/handler/GuiHandler.java
@@ -1,53 +1,62 @@
 package minechem.handler;
 
 import cpw.mods.fml.common.network.IGuiHandler;
+import minechem.apparatus.tier1.opticalMicroscope.OpticalMicroscopeTileEntity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 public class GuiHandler implements IGuiHandler
 {
-	/**
-	 * Get the GUI container object for the server
-	 *
-	 * @param ID     GUI element ID, unused
-	 * @param player Player entity
-	 * @param world  World object
-	 * @param x      World x coordinate
-	 * @param y      World y coordinate
-	 * @param z      World z coordinate
-	 * @return Container object for the TileEntity
-	 */
-	@Override
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-	{
-		TileEntity tileEntity = world.getTileEntity(x, y, z);
-		if (tileEntity != null)
-		{
-			// use instanceof to return the correct container object
-		}
-		return null;
-	}
+    /**
+     * Get the GUI container object for the server
+     *
+     * @param ID     GUI element ID, unused
+     * @param player Player entity
+     * @param world  World object
+     * @param x      World x coordinate
+     * @param y      World y coordinate
+     * @param z      World z coordinate
+     * @return Container object for the TileEntity
+     */
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+    {
+        TileEntity tileEntity = world.getTileEntity(x, y, z);
+        if (tileEntity != null)
+        {
+            // use instanceof to return the correct container object
+            if (tileEntity instanceof OpticalMicroscopeTileEntity)
+            {
 
-	/**
-	 * Get the GUI container object for the server
-	 *
-	 * @param ID     GUI element ID, unused
-	 * @param player Player entity
-	 * @param world  World object
-	 * @param x      World x coordinate
-	 * @param y      World y coordinate
-	 * @param z      World z coordinate
-	 * @return GUI object for the TileEntity
-	 */
-	@Override
-	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-	{
-		TileEntity tileEntity = world.getTileEntity(x, y, z);
-		if (tileEntity != null)
-		{
-			// use instanceof to return the correct GUI object
-		}
-		return null;
-	}
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get the GUI container object for the server
+     *
+     * @param ID     GUI element ID, unused
+     * @param player Player entity
+     * @param world  World object
+     * @param x      World x coordinate
+     * @param y      World y coordinate
+     * @param z      World z coordinate
+     * @return GUI object for the TileEntity
+     */
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+    {
+        TileEntity tileEntity = world.getTileEntity(x, y, z);
+        if (tileEntity != null)
+        {
+            // use instanceof to return the correct GUI object
+            if (tileEntity instanceof OpticalMicroscopeTileEntity)
+            {
+
+            }
+        }
+        return null;
+    }
 }
