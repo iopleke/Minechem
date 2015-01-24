bf381db303da51120997c6e05c299feb82ff6b64
jakimfett <jakimfett@picraft.com>
2015-01-23 17:41:06 -0800

    Add basic container object

---
+++ src/main/java/minechem/apparatus/prefab/container/BasicContainer.java
@@ -0,0 +1,42 @@
+package minechem.apparatus.prefab.container;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+
+public abstract class BasicContainer extends Container
+{
+    /**
+     * Determine if the player can interact with the container
+     *
+     * @param entityPlayer the player entity
+     * @return boolean
+     */
+    @Override
+    public boolean canInteractWith(EntityPlayer entityPlayer)
+    {
+        return true;
+    }
+
+    /**
+     * Add the player's inventory slots to the GUI
+     *
+     * @param inventoryPlayer the player's inventory
+     */
+    private void bindPlayerInventory(InventoryPlayer inventoryPlayer)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                addSlotToContainer(new Slot(inventoryPlayer, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+
+        for (int i = 0; i < 9; i++)
+        {
+            addSlotToContainer(new Slot(inventoryPlayer, i, 8 + i * 18, 142));
+        }
+    }
+}
