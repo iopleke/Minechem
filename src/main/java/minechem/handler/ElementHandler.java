8a94753cf35fc69dd3e0ffa0b990acd140deec73
yushijinhun <yushijinhun@gmail.com>
2015-01-24 14:49:58 +0800

    rewrite file accessing

--- src/main/java/minechem/handler/ElementHandler.java
+++ src/main/java/minechem/handler/ElementHandler.java
@@ -3,11 +3,16 @@ package minechem.handler;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+
 import org.apache.logging.log4j.Level;
+
 import minechem.Config;
 import minechem.helper.FileHelper;
 import minechem.helper.LogHelper;
@@ -20,50 +25,66 @@ public class ElementHandler
 {
     public void init()
     {
-        File elementsDataFile = new File(Compendium.Config.configPrefix + Compendium.Config.elementsDataJson);
-
-        if (Config.useDefaultElements||!elementsDataFile.isFile())
-        {
-            FileHelper.copyFromJar(ElementHandler.class, Compendium.Config.elementsDataJsonSource, Compendium.Config.elementsDataJson);
-        }
-
-        // Get the file again
-        elementsDataFile = new File(Compendium.Config.configPrefix + Compendium.Config.elementsDataJson);
-        if (elementsDataFile.isFile())
-        {
-            LogHelper.debug("JSON file exists");
-
-            JsonReader jReader=null;
-            try
-            {
-
-                jReader = new JsonReader(new InputStreamReader(new FileInputStream(elementsDataFile)));
-
-                JsonParser parser = new JsonParser();
-
-                JsonObject elementsObject = parser.parse(jReader).getAsJsonObject();
-                int count = 1;
-                while (elementsObject.has(Integer.toString(count)))
-                {
-//                    JsonObject element = elementsObject.get(Integer.toString(count)).getAsJsonObject();
-//                    LogHelper.debug("Atomic Number: " + count);
-//                    LogHelper.debug("Element name: " + element.get("longName"));
-//                    LogHelper.debug("Element abbreviation: " + element.get("shortName"));
-                    count++;
-
-                }
-                LogHelper.debug("Total of " + count + " elements loaded");
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			} finally {
-				if (jReader != null) {
-					try {
-						jReader.close();
+    	if (Config.useDefaultElements){
+    		InputStream stream=null;
+    		try{
+    			stream=FileHelper.getInputStreamFromJar(ElementHandler.class, Compendium.Config.elementsDataJsonSource);
+    			readFromStream(stream);
+    		} finally {
+    			if (stream!=null){
+    				try {
+						stream.close();
 					} catch (IOException e) {
-						LogHelper.exception(e, Level.WARN);
+						LogHelper.exception("Cannot close stream!", e, Level.WARN);
 					}
-				}
+    			}
+    		}
+		} else {
+			File elementsDataFile = new File(Compendium.Config.configPrefix + Compendium.Config.elementsDataJson);
+
+			if (!elementsDataFile.isFile()) {
+				FileHelper.copyFromJar(ElementHandler.class, Compendium.Config.elementsDataJsonSource, Compendium.Config.elementsDataJson);
 			}
-        }
-    }
+
+			// Get the file again
+			elementsDataFile = new File(Compendium.Config.configPrefix + Compendium.Config.elementsDataJson);
+			if (elementsDataFile.isFile()) {
+				LogHelper.debug("JSON file exists");
+				InputStream stream=null;
+				try {
+					stream=new FileInputStream(elementsDataFile);
+					readFromStream(stream);
+				} catch (FileNotFoundException e) {
+					throw new RuntimeException(e);
+				} finally {
+	    			if (stream!=null){
+	    				try {
+							stream.close();
+						} catch (IOException e) {
+							LogHelper.exception("Cannot close stream!", e, Level.WARN);
+						}
+	    			}
+	    		}
+			}
+		}
+	}
+
+	public void readFromStream(InputStream stream) {
+		JsonReader jReader = null;
+		jReader = new JsonReader(new InputStreamReader(stream));
+		JsonParser parser = new JsonParser();
+
+		JsonObject elementsObject = parser.parse(jReader).getAsJsonObject();
+		int count = 1;
+		while (elementsObject.has(Integer.toString(count))) {
+			// JsonObject element =
+			// elementsObject.get(Integer.toString(count)).getAsJsonObject();
+			// LogHelper.debug("Atomic Number: " + count);
+			// LogHelper.debug("Element name: " + element.get("longName"));
+			// LogHelper.debug("Element abbreviation: " +
+			// element.get("shortName"));
+			count++;
+		}
+		LogHelper.debug("Total of " + count + " elements loaded");
+	}
 }
--- src/main/java/minechem/helper/FileHelper.java
+++ src/main/java/minechem/helper/FileHelper.java
@@ -2,10 +2,14 @@ package minechem.helper;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+
 import minechem.Config;
 import minechem.reference.Compendium;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.Level;
 
 /**
  * Various functions to help with files
@@ -31,7 +35,7 @@ public class FileHelper
 			FileUtils.copyURLToFile(source, destination);
 		} catch (IOException e)
 		{
-			LogHelper.info("Couldn't load file from jar!");
+			LogHelper.exception("Couldn't load file from jar!", e, Level.WARN);
 			LogHelper.info("This is a bug, please report it to the mod author!");
 
 			if (Config.debugMode)
@@ -41,4 +45,22 @@ public class FileHelper
 		}
 	}
 
+	/**
+	 * Read a file from jar
+	 * 
+	 * @param classFromJar A class from the jar in question
+	 * @param file File to read, prepended with "/assets/minechem/"
+	 * @return the InputStream
+	 */
+	public static InputStream getInputStreamFromJar(Class<?> classFromJar, String file){
+		LogHelper.debug("Reading file "+file+" from jar");
+		URL url = classFromJar.getResource(Compendium.Config.assetPrefix + file);
+		try {
+			return url.openStream();
+		} catch (IOException e) {
+			LogHelper.exception("Couldn't read file from jar!", e, Level.WARN);
+			LogHelper.info("This is a bug, please report it to the mod author!");
+			throw new RuntimeException(e);
+		}
+	}
 }
