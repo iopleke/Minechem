ab553fbfaaf205fc4654a59b9ec11cff1b0a1f71
yushijinhun <yushijinhun@gmail.com>
2015-02-07 19:20:44 +0800

    gui element refactor

--- src/main/java/minechem/apparatus/prefab/gui/element/GuiFluidTank.java
+++ src/main/java/minechem/apparatus/prefab/gui/element/GuiFluidTank.java
@@ -5,12 +5,10 @@ import java.util.List;
 import minechem.helper.ColourHelper;
 import minechem.helper.LocalizationHelper;
 import minechem.reference.Compendium;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.fluids.FluidStack;
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
 
 /**
  * A tank to display in a GUI TODO: when there is an Object for a tank bind the tank directly to this Object
@@ -20,8 +18,6 @@ import org.lwjgl.opengl.GL12;
 public class GuiFluidTank extends GuiElement
 {
     private int capacity;
-    private int posX, posY;
-    private int width, height;
     private int colour;
 
     /**
@@ -35,11 +31,8 @@ public class GuiFluidTank extends GuiElement
      */
     public GuiFluidTank(int capacity, int posX, int posY, int width, int height)
     {
-        this.posX = posX;
-        this.posY = posY;
+        super(posX, posY, width, height);
         this.capacity = capacity;
-        this.width = width;
-        this.height = height;
         this.colour = ColourHelper.BLUE;
     }
 
@@ -61,7 +54,7 @@ public class GuiFluidTank extends GuiElement
      * @param colour in Integer form
      * @return the GuiFluidTank
      */
-    public GuiFluidTank setColour(int colour)
+    public GuiElement setColour(int colour)
     {
         this.colour = colour;
         return this;
@@ -153,65 +146,10 @@ public class GuiFluidTank extends GuiElement
         return x >= this.posX && x < this.posX + width - 2 && y >= this.posY && y < this.posY + height - 2;
     }
 
-    protected void drawHoveringText(List<String> tooltip, int x, int y, FontRenderer fontrenderer)
+    @Override
+    public void draw(int guiLeft, int guiTop)
     {
-        if (!tooltip.isEmpty())
-        {
-            GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-            GL11.glDisable(GL11.GL_LIGHTING);
-            GL11.glDisable(GL11.GL_DEPTH_TEST);
-            int k = 0;
-
-            for (String line : tooltip)
-            {
-                int l = fontrenderer.getStringWidth(line);
-
-                if (l > k)
-                {
-                    k = l;
-                }
-            }
-
-            int i1 = x + 12;
-            int j1 = y - 12;
-            int k1 = 8;
-
-            if (tooltip.size() > 1)
-            {
-                k1 += 2 + (tooltip.size() - 1) * 10;
-            }
-
-            this.zLevel = 300.0F;
-            int l1 = -267386864;
-            this.drawGradientRect(i1 - 3, j1 - 4, i1 + k + 3, j1 - 3, l1, l1);
-            this.drawGradientRect(i1 - 3, j1 + k1 + 3, i1 + k + 3, j1 + k1 + 4, l1, l1);
-            this.drawGradientRect(i1 - 3, j1 - 3, i1 + k + 3, j1 + k1 + 3, l1, l1);
-            this.drawGradientRect(i1 - 4, j1 - 3, i1 - 3, j1 + k1 + 3, l1, l1);
-            this.drawGradientRect(i1 + k + 3, j1 - 3, i1 + k + 4, j1 + k1 + 3, l1, l1);
-            int i2 = 1347420415;
-            int j2 = (i2 & 16711422) >> 1 | i2 & -16777216;
-            this.drawGradientRect(i1 - 3, j1 - 3 + 1, i1 - 3 + 1, j1 + k1 + 3 - 1, i2, j2);
-            this.drawGradientRect(i1 + k + 2, j1 - 3 + 1, i1 + k + 3, j1 + k1 + 3 - 1, i2, j2);
-            this.drawGradientRect(i1 - 3, j1 - 3, i1 + k + 3, j1 - 3 + 1, i2, i2);
-            this.drawGradientRect(i1 - 3, j1 + k1 + 2, i1 + k + 3, j1 + k1 + 3, j2, j2);
-
-            for (int k2 = 0; k2 < tooltip.size(); ++k2)
-            {
-                String s1 = tooltip.get(k2);
-                fontrenderer.drawStringWithShadow(s1, i1, j1, -1);
-
-                if (k2 == 0)
-                {
-                    j1 += 2;
-                }
-
-                j1 += 10;
-            }
-
-            this.zLevel = 0.0F;
-            GL11.glEnable(GL11.GL_LIGHTING);
-            GL11.glEnable(GL11.GL_DEPTH_TEST);
-            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        }
+        // TODO Auto-generated method stub
+        
     }
 }
