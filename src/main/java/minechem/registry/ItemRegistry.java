60271982bd4b3cef40488dc8c1671b5f2fe7b713
jakimfett <jakimfett@picraft.com>
2015-02-07 02:08:08 -0800

    Register the journal as an item

--- src/main/java/minechem/registry/ItemRegistry.java
+++ src/main/java/minechem/registry/ItemRegistry.java
@@ -1,10 +1,16 @@
 package minechem.registry;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import minechem.item.journal.JournalItem;
+
 public class ItemRegistry
 {
+    public static JournalItem journal;
 
     public static void init()
     {
+        journal = new JournalItem();
+        GameRegistry.registerItem(journal, journal.getUnlocalizedName());
     }
 
 }
