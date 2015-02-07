bab59c7ca0dd940937caf60967a2034939488a57
jakimfett <jakimfett@picraft.com>
2015-02-07 02:07:40 -0800

    Add class for the research journal

---
+++ src/main/java/minechem/item/journal/JournalItem.java
@@ -0,0 +1,14 @@
+package minechem.item.journal;
+
+import minechem.item.prefab.BasicItem;
+
+/**
+ *
+ */
+public class JournalItem extends BasicItem
+{
+    public JournalItem()
+    {
+        super("journal");
+    }
+}
