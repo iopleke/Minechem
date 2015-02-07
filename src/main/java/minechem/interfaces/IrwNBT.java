5427f0a45d7b63b586614dbd944d18bdf589ef97
way2muchnoise <jens.baetens@live.be>
2015-02-07 18:04:12 +0100

    add comment to IrwNBT

--- src/main/java/minechem/interfaces/IrwNBT.java
+++ src/main/java/minechem/interfaces/IrwNBT.java
@@ -2,6 +2,9 @@ package minechem.interfaces;
 
 import net.minecraft.nbt.NBTTagCompound;
 
+/**
+ * Can be applied to objects to define that they can be read and write to NBT
+ */
 public interface IrwNBT
 {
     public void readFromNBT(NBTTagCompound tag);
