9c539a6becebd80b5b9ac959ae09bd97e171d178
jakimfett <jakimfett@picraft.com>
2015-02-12 21:58:22 -0800

    Fix off-by-one error

--- src/main/java/minechem/item/journal/JournalGUI.java
+++ src/main/java/minechem/item/journal/JournalGUI.java
@@ -14,22 +14,22 @@ import org.lwjgl.opengl.GL11;
  */
 public class JournalGUI extends GuiScreen
 {
-    private PageBox[] currentPages, currentTitles;
-    private JournalPage[] pageIndex;
-    private int displayPage; // the left page
     private String[] authorList;
+    private PageBox[] currentPages, currentTitles;
+    private int displayPage; // the left page
+    private int fontSize = 1;
 
+    private int margin_l = 5;
+    private int margin_r = 5;
+    private int pageHeight = 800;
+    private JournalPage[] pageIndex;
     /**
      * FontBox values
      */
     private int pageWidth = 230;
-    private int pageHeight = 800;
-    private int titleWidth = 230;
-    private int titleHeight = 100;
-    private int margin_l = 5;
-    private int margin_r = 5;
     private int space = 1;
-    private int fontSize = 1;
+    private int titleHeight = 100;
+    private int titleWidth = 230;
 
     /**
      *
@@ -44,6 +44,42 @@ public class JournalGUI extends GuiScreen
         currentTitles = new PageBox[2];
         displayPage = 0;
         loadPage(displayPage);
+    }
+
+    @Override
+    public boolean doesGuiPauseGame()
+    {
+        return false;
+    }
+
+    private void drawFoldedPages()
+    {
+        if (displayPage > 1)
+        {
+            // Draw folded page on the left
+            GuiDraw.drawTexturedModalRect(5, 163, 0, 188, 21, 21);
+        }
+        if (displayPage + 2 < pageIndex.length)
+        {
+            // Draw folded page on the right
+            GuiDraw.drawTexturedModalRect(230, 160, 21, 188, 21, 21);
+        }
+    }
+
+    private void drawJournalBackground()
+    {
+        GuiDraw.drawTexturedModalRect(0, 0, 0, 0, 256, 188);
+    }
+
+    /**
+     * Draw the actual page content
+     */
+    private void drawPages()
+    {
+        FontBoxHelper.renderPageBox(currentPages[0], 10, 30, zLevel);
+        FontBoxHelper.renderPageBox(currentTitles[0], 20, 10, zLevel);
+        FontBoxHelper.renderPageBox(currentPages[1], 10 + 128, 30, zLevel);
+        FontBoxHelper.renderPageBox(currentTitles[1], 20 + 128, 10, zLevel);
     }
 
     @Override
@@ -63,40 +99,39 @@ public class JournalGUI extends GuiScreen
     }
 
     /**
-     * Draw the actual page content
+     *
+     * @param c
+     * @param keycode
      */
-    private void drawPages()
-    {
-        FontBoxHelper.renderPageBox(currentPages[0], 10, 30, zLevel);
-        FontBoxHelper.renderPageBox(currentTitles[0], 20, 10, zLevel);
-        FontBoxHelper.renderPageBox(currentPages[1], 10 + 128, 30, zLevel);
-        FontBoxHelper.renderPageBox(currentTitles[1], 20 + 128, 10, zLevel);
-    }
-
     @Override
-    public boolean doesGuiPauseGame()
+    protected void keyTyped(char c, int keycode)
     {
-        return false;
-    }
-
-    private void drawJournalBackground()
-    {
-        GuiDraw.drawTexturedModalRect(0, 0, 0, 0, 256, 188);
-    }
-
-    private void drawFoldedPages()
-    {
-
-        if (displayPage > 0)
+        if (keycode == Keyboard.KEY_LEFT)
         {
-            // Draw folded page on the left
-            GuiDraw.drawTexturedModalRect(5, 163, 0, 188, 21, 21);
+            if (displayPage > 0)
+            {
+                displayPage--;
+                if (displayPage > 0)
+                {
+                    displayPage--;
+                }
+            }
         }
-        if (displayPage + 2 < pageIndex.length)
+        if (keycode == Keyboard.KEY_RIGHT)
         {
-            // Draw folded page on the right
-            GuiDraw.drawTexturedModalRect(230, 160, 21, 188, 21, 21);
+            if (displayPage < pageIndex.length - 1)
+            {
+                displayPage++;
+                if (displayPage < pageIndex.length - 1)
+                {
+                    displayPage++;
+                }
+            }
         }
+
+        loadPage(displayPage);
+        super.keyTyped(c, keycode);
+
     }
 
     /**
@@ -134,41 +169,5 @@ public class JournalGUI extends GuiScreen
                 currentTitles[0] = FontBoxHelper.boxText(pageIndex[displayPage - 1].title, titleWidth, titleHeight, margin_l, margin_r, space, fontSize);
             }
         }
-    }
-
-    /**
-     *
-     * @param c
-     * @param keycode
-     */
-    @Override
-    protected void keyTyped(char c, int keycode)
-    {
-        if (keycode == Keyboard.KEY_LEFT)
-        {
-            if (displayPage > 0)
-            {
-                displayPage--;
-                if (displayPage > 0)
-                {
-                    displayPage--;
-                }
-            }
-        }
-        if (keycode == Keyboard.KEY_RIGHT)
-        {
-            if (displayPage < pageIndex.length)
-            {
-                displayPage++;
-                if (displayPage < pageIndex.length)
-                {
-                    displayPage++;
-                }
-            }
-        }
-
-        loadPage(displayPage);
-        super.keyTyped(c, keycode);
-
     }
 }
