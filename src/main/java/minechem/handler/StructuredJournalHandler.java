cca2a6d2fde803c6d87ff73e2797ad3eec3890c9
jakimfett <jakimfett@picraft.com>
2015-02-18 20:51:42 -0800

    Fix EntryPage can't be cast to SectionPage

--- src/main/java/minechem/handler/StructuredJournalHandler.java
+++ src/main/java/minechem/handler/StructuredJournalHandler.java
@@ -5,6 +5,13 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 import cpw.mods.fml.common.registry.GameRegistry;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import minechem.Compendium;
 import minechem.Config;
 import minechem.helper.FileHelper;
@@ -23,17 +30,8 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.JsonToNBT;
-import net.minecraft.nbt.NBTException;
 import net.minecraft.nbt.NBTTagCompound;
 import org.apache.logging.log4j.Level;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class StructuredJournalHandler
 {
@@ -62,9 +60,14 @@ public class StructuredJournalHandler
         JsonReader jReader = new JsonReader(new InputStreamReader(inputStream));
         JsonParser parser = new JsonParser();
 
-        SectionPage journal = (SectionPage) getPageFromJSONObject("", "", parser.parse(jReader).getAsJsonObject());
-        LogHelper.info("Total of " + journal.getSubPages() + " pages registered");
-        JournalRegistry.journal = journal;
+        IJournalPage testJournal = getPageFromJSONObject("", "", parser.parse(jReader).getAsJsonObject());
+        if (testJournal instanceof SectionPage)
+        {
+
+            SectionPage journal = (SectionPage) testJournal;
+            LogHelper.info("Total of " + journal.getSubPages() + " pages registered");
+            JournalRegistry.journal = journal;
+        }
     }
 
     public static IJournalPage getPageFromJSONObject(String name, String chapter, JsonObject object)
@@ -72,13 +75,17 @@ public class StructuredJournalHandler
         IJournalPage result = new SectionPage(name);
         if ("journal".equals(name) && chapter.isEmpty())
         {
-            for (IJournalPage page : getPagesFromJsonObject("", object)) result.addSubPage(page);
-        }
-        else if (object.has("section"))
+            for (IJournalPage page : getPagesFromJsonObject("", object))
+            {
+                result.addSubPage(page);
+            }
+        } else if (object.has("section"))
         {
-            for (IJournalPage page : getPagesFromJsonObject((chapter.isEmpty() ? "" : chapter + ".") + name, object.getAsJsonObject("section"))) result.addSubPage(page);
-        }
-        else
+            for (IJournalPage page : getPagesFromJsonObject((chapter.isEmpty() ? "" : chapter + ".") + name, object.getAsJsonObject("section")))
+            {
+                result.addSubPage(page);
+            }
+        } else
         {
             List<IJournalElement> elements = getElementsFromJsonObject((chapter.isEmpty() ? "" : chapter + ".") + name, object);
             result = new EntryPage(name, chapter, elements);
@@ -107,8 +114,7 @@ public class StructuredJournalHandler
         if (entrySet.size() == 0)
         {
             pages.add(new JournalText(page));
-        }
-        else
+        } else
         {
             for (Map.Entry<String, JsonElement> elementEntry : entrySet)
             {
@@ -116,8 +122,7 @@ public class StructuredJournalHandler
                 if (element != null)
                 {
                     pages.add(element);
-                }
-                else
+                } else
                 {
                     LogHelper.info(page + " object " + elementEntry.getKey() + " failed to parse");
                 }
@@ -128,7 +133,10 @@ public class StructuredJournalHandler
 
     public static IJournalElement getElementFromJsonElement(String page, String key, JsonElement element)
     {
-        if (element.isJsonNull()) return new JournalText(page, page + "." + key);
+        if (element.isJsonNull())
+        {
+            return new JournalText(page, page + "." + key);
+        }
         if (element.isJsonObject())
         {
             return getElementFromJsonObject(page, key, element.getAsJsonObject());
@@ -137,8 +145,9 @@ public class StructuredJournalHandler
         {
             String unlockString = element.getAsString();
             return new JournalText(unlockString, page + "." + key);
+        } catch (Exception e)
+        {
         }
-        catch (Exception e){}
         return null;
     }
 
@@ -152,27 +161,38 @@ public class StructuredJournalHandler
         {
             unlock = object.get("unlock").getAsString();
         }
-        if (object.entrySet().size() == 0) return new JournalText(page, page + "." + key);
-        if (object.entrySet().size() == 1 && object.has("unlock")) return new JournalText(unlock, page + "." + key);
+        if (object.entrySet().size() == 0)
+        {
+            return new JournalText(page, page + "." + key);
+        }
+        if (object.entrySet().size() == 1 && object.has("unlock"))
+        {
+            return new JournalText(unlock, page + "." + key);
+        }
         if (object.has("width"))
         {
             int width = object.get("width").getAsInt();
             int height = object.get("height").getAsInt();
-            AlignmentMode align = object.has("align")? AlignmentMode.valueOf(object.get("align").getAsString()) : AlignmentMode.JUSTIFY;
-            FloatMode floatMode = object.has("float")? FloatMode.valueOf(object.get("float").getAsString()) : FloatMode.NONE;
+            AlignmentMode align = object.has("align") ? AlignmentMode.valueOf(object.get("align").getAsString()) : AlignmentMode.JUSTIFY;
+            FloatMode floatMode = object.has("float") ? FloatMode.valueOf(object.get("float").getAsString()) : FloatMode.NONE;
             if (object.has("image") || key.endsWith(".png"))
             {
-                String imageDir = object.has("image")? object.get("image").getAsString(): key;
-                if (!imageDir.startsWith(prefix)) imageDir = prefix + imageDir;
-                if (!imageDir.endsWith(suffix)) imageDir += suffix;
+                String imageDir = object.has("image") ? object.get("image").getAsString() : key;
+                if (!imageDir.startsWith(prefix))
+                {
+                    imageDir = prefix + imageDir;
+                }
+                if (!imageDir.endsWith(suffix))
+                {
+                    imageDir += suffix;
+                }
                 return new JournalImage(unlock, imageDir, width, height, align, floatMode);
-            }
-            else
+            } else
             {
                 ItemStack stack = null;
                 String id;
                 String[] split;
-                int damage = object.has("damage")? object.get("damage").getAsInt():0;
+                int damage = object.has("damage") ? object.get("damage").getAsInt() : 0;
                 NBTTagCompound tagCompound;
                 try
                 {
@@ -189,21 +209,19 @@ public class StructuredJournalHandler
                         return null;
                     }
                     split = id.split(":");
-                    if (split.length!=2)
+                    if (split.length != 2)
                     {
                         return null;
                     }
-                    Item item = GameRegistry.findItem(split[0],split[1]);
-                    if (item!=null)
+                    Item item = GameRegistry.findItem(split[0], split[1]);
+                    if (item != null)
                     {
                         stack = new ItemStack(item, 1, damage);
-                    }
-                    else
+                    } else
                     {
                         stack = GameRegistry.findItemStack(split[0], split[1], 1);
                     }
-                }
-                else if (object.has("block"))
+                } else if (object.has("block"))
                 {
                     id = object.get("block").getAsString();
                     if (!id.contains(":"))
@@ -211,23 +229,25 @@ public class StructuredJournalHandler
                         return null;
                     }
                     split = id.split(":");
-                    if (split.length!=2)
+                    if (split.length != 2)
                     {
                         return null;
                     }
                     Block block = GameRegistry.findBlock(split[0], split[1]);
                     if (block != null)
                     {
-                        stack = new ItemStack(block, 1, damage%16);
+                        stack = new ItemStack(block, 1, damage % 16);
                     }
-                }
-                else if (object.has("chemical"))
+                } else if (object.has("chemical"))
                 {
                     stack = Jenkins.getStack(object.get("chemical").getAsString());
                 }
-                if (stack!=null)
+                if (stack != null)
                 {
-                    if (tagCompound != null) stack.setTagCompound(tagCompound);
+                    if (tagCompound != null)
+                    {
+                        stack.setTagCompound(tagCompound);
+                    }
                     return new JournalImage(unlock, stack, width, height, align, floatMode);
                 }
             }
