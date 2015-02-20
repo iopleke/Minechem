b2827349ae082b7dbf84dfc8f6f2a79ec8faa6e9
jakimfett <jakimfett@picraft.com>
2015-02-19 21:22:54 -0800

    Remove unneeded import

--- src/main/java/minechem/apparatus/prefab/tileEntity/BasicInventoryTileEntity.java
+++ src/main/java/minechem/apparatus/prefab/tileEntity/BasicInventoryTileEntity.java
@@ -7,7 +7,6 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.Constants;
 
 /**
