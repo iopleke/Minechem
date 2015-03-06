6e13a461407c25e29d8acb96702777a5d42b6096
way2muchnoise <jens.baetens@live.be>
2015-03-06 19:24:14 +0100

    improves toString in the StringHelper
    += in for loops are slow

--- src/main/java/minechem/helper/StringHelper.java
+++ src/main/java/minechem/helper/StringHelper.java
@@ -11,12 +11,18 @@ public class StringHelper
         return stringRows.size() * fontRenderer.FONT_HEIGHT;
     }
 
+    /**
+     * Concat all objects with given separator
+     * @param separator eg. ", "
+     * @param objects
+     * @return one string
+     */
     public static String toString(String separator, Object... objects)
     {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (Object object : objects)
         {
-            result += String.valueOf(object) + separator;
+            result.append(String.valueOf(object)).append(separator);
         }
         return result.substring(0, result.length() - separator.length());
     }
